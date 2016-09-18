bd3fb27d1b45de51367ebaedbc286aca446eb597
EtienneCmb <e.combrisson@gmail.com>
2016-09-18 10:51:33 -0400

    Fix colorbar for the example

--- examples/5_RealDataExample.py
+++ examples/5_RealDataExample.py
@@ -9,5 +9,5 @@ data = np.load('RealDataExample.npz')
 s_data = data['beta']
 s_xyz = data['xyz']
 
-vb = vbrain(s_xyz=s_xyz, s_data=s_data)
+vb = vbrain(s_xyz=s_xyz, s_data=s_data, cmap_vmin=-1, cmap_vmax=1, cmap='jet')
 vb.show()
