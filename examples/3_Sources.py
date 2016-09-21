8da72d46decb6a25d45fc36e2f7399469c6cda00
EtienneCmb <e.combrisson@gmail.com>
2016-09-20 20:44:32 -0400

    Fix colormap

--- examples/3_Sources.py
+++ examples/3_Sources.py
@@ -28,11 +28,11 @@ s_edgewidth = 1				# Width of the edges
 # Navigate to the menu to transform -> cortical projection (or CTRL+P)
 # then, the activity of deep sources can be project on the surface. 
 # To make a pretty plot, we can define some colormap properties :
-cmap = 'viridis'				# Matplotlib colormap
-cmap_vmin = -40					# Define a minimum
-cmap_vmax = 21					# Define a maximum
-cmap_under = 'gray'				# Every values under vmin are going to be gray
-cmap_over = (0.1,0.1,0.1, 1)	# Every values over vmax are going to be black
+s_cmap = 'viridis'				# Matplotlib colormap
+s_cmap_vmin = -40					# Define a minimum
+s_cmap_vmax = 21					# Define a maximum
+s_cmap_under = 'gray'				# Every values under vmin are going to be gray
+s_cmap_over = (0.1,0.1,0.1, 1)	# Every values over vmax are going to be black
 
 # If you want to customize the colormap, display the quick settings menu (or CTRL+D)
 # and checked the button 'See live changement of colormap settings'
@@ -45,7 +45,7 @@ s_textsize = 2				# Size of the text
 s_textshift = (1.5,1.5,0)	# To avoid a superposition between the text and sources sphere, we move the text of (x, y, z) points
 
 vb = vbrain(s_xyz=s_xyz, s_color=s_color, s_data=s_data, s_radiusmin=s_radiusmin, s_radiusmax=s_radiusmax, s_opacity=s_opacity,
-            cmap=cmap, cmap_vmin=cmap_vmin, cmap_vmax=cmap_vmax, cmap_under=cmap_under, cmap_over=cmap_over, cb_label=cb_label,
+            s_cmap=s_cmap, s_cmap_vmin=s_cmap_vmin, s_cmap_vmax=s_cmap_vmax, s_cmap_under=s_cmap_under, s_cmap_over=s_cmap_over, cb_label=cb_label,
             s_text=s_text, s_textcolor=s_textcolor, s_textsize=s_textsize, s_textshift=s_textshift, s_edgecolor=s_edgecolor,
             s_edgewidth=s_edgewidth, a_template='B1')
 vb.show()
