608e5d7d0a1baef0c7f665486acf51cda6309d5b
EtienneCmb <e.combrisson@gmail.com>
2016-09-28 00:51:10 -0400

    Update area management

--- vbrain/interface/uiElements/uiArea.py
+++ vbrain/interface/uiElements/uiArea.py
@@ -12,23 +12,91 @@ class uiArea(object):
     def __init__(self,):
         # By default, hide panels :
         self.structPanel.hide()
-        self.structPanelSelect.hide()
+        self.struct_rmvLst.hide()
 
         # Get plot properties :
-        self.structEnable.clicked.connect(self.fcn_buildStruct)
+        self.structEnable.clicked.connect(self.fcn_show_hide_struct_panel)
         self.Sub_brod.clicked.connect(self.fcn_buildStruct)
         self.Sub_aal.clicked.connect(self.fcn_buildStruct)
-        self.struct_all.clicked.connect(self.fcn_buildStruct)
-        self.struct_specific.clicked.connect(self.fcn_buildStruct)
+
+        # List management :
+        self.struct2select.itemClicked.connect(self.fcn_lst_add)
+        self.struct2add.itemClicked.connect(self.fcn_lst_rmv)
+        self.struct_addLst.clicked.connect(self.fcn_add_struct)
+        self.struct_rmvLst.clicked.connect(self.fcn_rmv_struct)
+        self.struct_rstLst.clicked.connect(self.fcn_rst_struct)
+        self._struct2add = []
 
         # Internal/external projection :
         self.struct_External.clicked.connect(self.fcn_InternalExternal)
         self.struct_Internal.clicked.connect(self.fcn_InternalExternal)
 
         # System :
-        self.strcutShow.clicked.connect(self.fcn_visble_area)
+        self.strcutShow.clicked.connect(self.fcn_visible_area)
         self.struct_apply.clicked.connect(self.fcn_applyStruct)
 
+        self.fcn_buildStruct()
+
+
+
+    #########################################################################
+    # MANAGE LIST
+    #########################################################################
+    # List click (show/hide buttons) :
+    def fcn_lst_add(self):
+        """
+        """
+        # Manage buttons :
+        self.struct_rmvLst.hide()
+        self.struct_addLst.show()
+
+
+    def fcn_lst_rmv(self):
+        """
+        """
+        # Manage buttons :
+        self.struct_addLst.hide()
+        self.struct_rmvLst.show()
+
+
+    # Add/remove elements to the list :
+    def fcn_add_struct(self):
+        """
+        """
+        currentItem = [k.text() for k in self.struct2select.selectedItems()]
+        for k in currentItem:
+            if k not in self._struct2add:
+                self._struct2add.append(k)
+            self.fcn_update_list()
+
+
+    def fcn_rmv_struct(self):
+        """
+        """
+        currentItem = self.struct2add.currentItem().text()
+        self._struct2add.pop(self._struct2add.index(currentItem));
+        self.fcn_update_list()
+
+
+    def fcn_update_list(self):
+        """
+        """
+        # Update list :
+        self.struct2add.clear()
+        self._struct2add.sort()
+        self.struct2add.addItems(self._struct2add)
+
+
+    def fcn_rst_struct(self):
+        """
+        """
+        self.struct2add.clear()
+        self._struct2add = []
+
+
+    #########################################################################
+    # MANAGE LIST
+    #########################################################################
     def fcn_buildStruct(self):
         """
         """
@@ -38,23 +106,12 @@ class uiArea(object):
         elif self.Sub_aal.isChecked():
             self.area.structure = 'aal'
 
-        # Get color/colormap properties :
-        if self.structUniform.isChecked():
-            self.area.color = 'white'
-        elif self.struct_specific.isChecked():
-            pass
-            # self.area.color = self.struct_color_edit.currentText()
-
         # Update list of structures :
-        self.struct_list.clear()
-        self.struct_list.addItems(self.area._label)
-
-        # Get selected areas :
-        if self.struct_all.isChecked():
-            self.area.select = None
-        elif struct_specific.isChecked():
-            self.area.select = []
-        self.area.select = [4, 6, 32, 40]
+        self.struct2select.clear()
+        self.struct2select.addItems(self.area._label)
+        self.fcn_rst_struct()
+        self.area.color = 'white' # <-----------------------------------------------
+        self.area.select = [4, 6, 32, 40] # <-----------------------------------------------
             
         # Reconstruct brodmann structure :
 
@@ -63,6 +120,10 @@ class uiArea(object):
     def fcn_applyStruct(self):
         """
         """
+        struct2add = [int(k.split(':')[0]) for k in self._struct2add]
+        struct2add.sort()
+        self.area.select = struct2add
+        print(self.area.select)
         self.area._get_vertices()
         self.area._plot()
         self.area.mesh.parent = self._vbNode
@@ -78,7 +139,16 @@ class uiArea(object):
             self.area.mesh.projection('external')
 
 
-    def fcn_visble_area(self):
+    #########################################################################
+    # PANEL AND VISIBILITY
+    #########################################################################
+    def fcn_show_hide_struct_panel(self):
+        """
+        """
+        self.structPanel.setVisible(self.structEnable.isChecked())
+
+
+    def fcn_visible_area(self):
         """
         """
         self.area.mesh.visible = self.strcutShow.isChecked()
