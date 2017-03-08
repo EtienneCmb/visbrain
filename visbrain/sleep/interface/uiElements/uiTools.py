e838127087d6b66f7f217f86798e14b12dc5a193
EtienneCmb <e.combrisson@gmail.com>
2017-03-08 16:03:44 +0100

    Merge detection methods

--- visbrain/sleep/interface/uiElements/uiTools.py
+++ visbrain/sleep/interface/uiElements/uiTools.py
@@ -1,6 +1,6 @@
 """Main class for sleep tools managment."""
 
-from ....utils import detection
+from ....utils import remdetect, spindlesdetect
 
 __all__ = ['uiTools']
 
@@ -19,74 +19,72 @@ class uiTools(object):
         # =====================================================================
 
         # =====================================================================
-        # PEAK DETECTION
+        # REM / SPINDLES / DETECTION
         # =====================================================================
-        # Add list of channels :
-        self._ToolPeakChan.addItems(self._channels)
-        # Connect functions :
-        self._ToolPeakChan.currentIndexChanged.connect(self._fcn_peakdetect)
-        self._ToolPeakLook.valueChanged.connect(self._fcn_peakdetect)
-        self._ToolPeakLook.setKeyboardTracking(False)
-        self._ToolPeakMinMax.currentIndexChanged.connect(self._fcn_peakdetect)
+        # Commonth elements :
+        self._ToolDetectChan.addItems(self._channels)
+        self._ToolDetectType.currentIndexChanged.connect(
+                                                     self._fcn_switchDetection)
+        self._ToolDetectApply.clicked.connect(self._fcn_applyDetection)
+        self._fcn_switchDetection()
 
-        # =====================================================================
-        # REM DETECTION
-        # =====================================================================        
-        # Add list of channels :
-        self._ToolRemChan.addItems(self._channels)
+        # -------------------------------------------------
+        # REM detection :
         self._ToolRemTh.setValue(3.)
-        # Connect functions
-        self._ToolRemChan.currentIndexChanged.connect(self._fcn_remdetect)
-        self._ToolRemTh.valueChanged.connect(self._fcn_remdetect)
-        self._ToolRemTh.setKeyboardTracking(False)
-        
-        # =====================================================================
-        # SPINDLES DETECTION
-        # =====================================================================
-        # Add list of channels :
-        self._ToolSpinChan.addItems(self._channels)
+
+        # -------------------------------------------------
+        # Spindles detection :
         self._ToolSpinTh.setValue(4.)
-        # Connect functions
-        self._ToolSpinChan.currentIndexChanged.connect(self._fcn_spindlesdetect)
-        self._ToolSpinTh.valueChanged.connect(self._fcn_spindlesdetect)
-        self._ToolSpinTh.setKeyboardTracking(False)
 
     # =====================================================================
-    # PEAK DETECTION
+    # DETECTION
     # =====================================================================
-    def _fcn_peakdetect(self):
-        """Perform and display a peak detection on the specified channel."""
-        # Get variables :
-        idx = self._ToolPeakChan.currentIndex()
-        look = self._ToolPeakLook.value()
-        disp = self._ToolPeakMinMax.currentIndex()
-        disp_types = ['max', 'min', 'minmax']
-        # Set data :
-        self._peak.set_data(self._data[idx], self._time, self._chan.peak[idx],
-                            disp_types[disp], look)
-        if not self.canvas_isVisible(idx):
-            self.canvas_setVisible(idx, True)
+    def _fcn_switchDetection(self):
+        """Switch between detection types (show / hide panels)."""
+        # Define ref :
+        ref = ['REM', 'Spindles', 'Peaks']
+        # Get current selected text :
+        viz = [self._ToolDetectType.currentText() == k for k in ref]
+        # Set widget visibility :
+        _ = [k.setVisible(i) for k, i in zip([self._ToolRemPanel,
+                                             self._ToolSpinPanel,
+                                             self._ToolPeakPanel], viz)]
 
-    # =====================================================================
-    # REM DETECTION
-    # =====================================================================
-    def _fcn_remdetect(self):
-        """Perform and display a rem detection on the specified channel."""
-        # Get variables :
-        idx = self._ToolRemChan.currentIndex()
-        thr = self._ToolRemTh.value()
-        # Set data
+    def _fcn_applyDetection(self):
+        """Apply detection (either REM / Spindles / Peaks."""
+        # Get current channel and detection method :
+        idx = self._ToolDetectChan.currentIndex()
+        method = self._ToolDetectType.currentText()
+
+        # Switch between detection types :
+        # ------------------- REM -------------------
+        if method == 'REM':
+            # Get variables :
+            thr = self._ToolRemTh.value()
+            # Get REM indices :
+            # Set them to ChannelPlot object :
+            # Update plot :
+            self._fcn_sliderMove()
+
+        # ------------------- SPINDLES -------------------
+        elif method == 'Spindles':
+            # Get variables :
+            thr = self._ToolSpinTh.value()
+            # Get Spindles indices :
+            # Set them to ChannelPlot object :
+            # Update plot :
+            self._fcn_sliderMove()
+
+        # ------------------- PEAKS -------------------
+        elif method == 'Peaks':
+            # Get variables :
+            look = self._ToolPeakLook.value()
+            disp = self._ToolPeakMinMax.currentIndex()
+            disp_types = ['max', 'min', 'minmax']
+            # Set data :
+            self._peak.set_data(self._data[idx], self._time,
+                                self._chan.peak[idx], disp_types[disp], look)
+
+        # Be sure panel is displayed :
         if not self.canvas_isVisible(idx):
             self.canvas_setVisible(idx, True)
-            
-    # =====================================================================
-    # SPINDLES DETECTION
-    # =====================================================================
-    def _fcn_spindlesdetect(self):
-        """Perform and display a spindles detection on the specified channel."""
-        # Get variables :
-        idx = self._ToolSpinChan.currentIndex()
-        thr = self._ToolSpinTh.value()
-        # Set data
-        if not self.canvas_isVisible(idx):
-            self.canvas_setVisible(idx, True)       
