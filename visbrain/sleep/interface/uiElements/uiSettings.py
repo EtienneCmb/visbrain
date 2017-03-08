7eef6ad5c10bcb210a6eb29a5f4a5aa47a913418
EtienneCmb <e.combrisson@gmail.com>
2017-03-08 12:07:55 +0100

    Remove print

--- visbrain/sleep/interface/uiElements/uiSettings.py
+++ visbrain/sleep/interface/uiElements/uiSettings.py
@@ -297,5 +297,4 @@ class uiSettings(object):
 
     def on_mouse_wheel(self, event):
         """Executed function on mouse wheel."""
-        print(event.delta[1])
         self._SlVal.setValue(self._SlVal.value() + event.delta[1])
