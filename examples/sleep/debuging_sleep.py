67d02a47f5da6c17c113da4321d6c89751067b52
EtienneCmb <e.combrisson@gmail.com>
2017-03-09 23:06:59 +0100

    Switch between different examples

--- examples/sleep/debuging_sleep.py
+++ examples/sleep/debuging_sleep.py
@@ -4,34 +4,65 @@ import numpy as np
 from visbrain import Sleep
 from scipy.io import loadmat
 
-sf = 100.
-#############################################################################
-# REAL DATA EXAMPLE :
-mat = loadmat('testing_database.mat')
+torun = 'big'
 
-data = np.ascontiguousarray(mat['x'], dtype=np.float32)
-hypnot = mat['hypno'].ravel()
-channels = [str(k[0]) for k in mat['labels'][0][0:-2]]
+###########################################################################
+# LOAD BY GUI :
+if torun == 'none':
+    file = None
+    hypno_file = None
+    data = None
+    hypno = None
+    channels = None
+    sf = None
 
-hypno = []
-for k in hypnot:
-    hypno += [k] * 100
-hypno = np.ascontiguousarray(hypno, dtype=np.float32)
-#############################################################################
+###########################################################################
+# LOAD BY DATASET :
+elif torun == 'small':
+    sf = 100.
+    mat = loadmat('testing_database.mat')
+
+    data = np.ascontiguousarray(mat['x'], dtype=np.float32)
+    hypnot = mat['hypno'].ravel()
+    channels = [str(k[0]) for k in mat['labels'][0][0:-2]]
+
+    hypno = []
+    for k in hypnot:
+        hypno += [k] * 100
+    hypno = np.ascontiguousarray(hypno, dtype=np.float32)
+
+    file = None
+    hypno_file = None
+
+###########################################################################
+# LOAD BY PATH :
+elif torun == 'big':
+    file = '/home/etienne/wetransfer-ff7857/s101_sleep.eeg'
+    hypno_file = '/home/etienne/wetransfer-ff7857/s101_jbe.hyp'
+    data = None
+    channels = None
+    sf = None
+    hypno = None
+
+###########################################################################
 # DEBUGGING EXAMPLE :
-# sf = 512.
-# nelec = 20
-# npts = 14745600
-# nelec = 3
-# npts = 1000000
+elif torun == 'rnd':
+    sf = 512.
+    nelec = 3
+    npts = 100000
 
-# data = 10*np.random.rand(nelec, npts)
-# channels = ['channel_'+str(k) for k in range(nelec)]
-# hypno = np.random.rand(npts)
+    data = 10*np.random.rand(nelec, npts)
+    channels = ['channel_'+str(k) for k in range(nelec)]
+    hypno = np.random.rand(npts)
+
+    file = None
+    hypno_file = None
 
 
-Sleep(data=data, channels=channels, sf=sf, downsample=100., hypno=hypno,
-      line='gl', axis=False).show()
+sp = Sleep(file=file, hypno_file=hypno_file, data=data, channels=channels,
+           sf=sf, downsample=100., hypno=hypno, line='gl', axis=False)
+sp._fcn_applyDetection()
+sp.show()
 # s = Sleep()
 # s.show()
 
