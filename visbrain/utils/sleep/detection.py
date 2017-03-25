94fee98b2adfd684affd75f769ed1d1fc6637c32
EtienneCmb <e.combrisson@gmail.com>
2017-03-25 22:10:48 +0100

    Add event.py file and move related functions

--- visbrain/utils/sleep/detection.py
+++ visbrain/utils/sleep/detection.py
@@ -14,6 +14,8 @@ from scipy.signal import hilbert, daub
 
 from ..filtering import filt, morlet, morlet_power
 from ..sigproc import movingaverage, derivative, tkeo, soft_thresh
+from .event import (_events_duration, _events_removal, _events_distance_fill,
+                    _events_mean_freq)
 
 __all__ = ['peakdetect', 'remdetect', 'spindlesdetect', 'slowwavedetect',
            'kcdetect']
@@ -135,22 +137,20 @@ def kcdetect(elec, sf, threshold, hypno, nrem_only):
         proba[good_idx] *= 2
 
         if hypLoaded:
-            proba[np.where(hypno == 0)[0]] *= 0.2
-            proba[np.where(hypno == 2)[0]] *= 3
-            proba[np.where(hypno == 3)[0]] *= 0.5
-            proba[np.where(hypno == 4)[0]] *= 0.2
-            proba[np.where(hypno == -1)[0]] *= 0.5
+            proba[np.where(hypno == 0)[0]] *= .2
+            proba[np.where(hypno == 2)[0]] *= 3.
+            proba[np.where(hypno == 3)[0]] *= .5
+            proba[np.where(hypno == 4)[0]] *= .2
+            proba[np.where(hypno == -1)[0]] *= .5
 
         idx_sup_thr = np.intersect1d(idx_sup_thr, np.where(proba >= 0.3)[0])
 
         # K-COMPLEX MORPHOLOGY
         # Get where KC start / end and duration :
-        _, _, idx_start, idx_stop = _events_duration(
-            idx_sup_thr, sf)
+        _, _, idx_start, idx_stop = _events_duration(idx_sup_thr, sf)
 
         # Get where min_dur <  KC duration < max_dur :
-        _, duration_ms, idx_start, idx_stop = _events_duration(
-            idx_sup_thr, sf)
+        _, duration_ms, idx_start, idx_stop = _events_duration(idx_sup_thr, sf)
         good_dur = np.where(np.logical_and(duration_ms > tMin,
                                            duration_ms < tMax))[0]
 
@@ -620,142 +620,3 @@ def _datacheck(x_axis, y_axis):
     y_axis = np.array(y_axis)
     x_axis = np.array(x_axis)
     return x_axis, y_axis
-
-
-###########################################################################
-# INDEX MANIPULATION FUNCTIONS
-###########################################################################
-
-
-def _events_duration(index, sf):
-    """Compute spindles/REM duration in ms.
-
-    Args:
-        index: np.ndarray
-            Index of events locations.
-
-        sf: float
-            The sampling frequency.
-
-    Returns:
-        number: int
-            Number of events
-
-        duration_ms: np.ndarray
-            Duration of each event
-
-        idx_start: np.ndarray
-            Array of integers where each event begin.
-
-        idx_end: np.ndarray
-            Array of integers where each event finish.
-    """
-    # Find boolean values where each spindle start :
-    bool_break = (index[1:] - index[:-1]) != 1
-    # Get spindles number :
-    number = bool_break.sum()
-    # Build starting / ending spindles index :
-    idx_start = np.hstack([np.array([0]), np.where(bool_break)[0] + 1])
-    idx_stop = np.hstack((idx_start[1::], len(index)))
-    # Compute duration :
-    duration_ms = np.diff(idx_start) * (1000. / sf)
-
-    return number, duration_ms, idx_start, idx_stop
-
-
-def _events_removal(idx_start, idx_stop, good_dur):
-    """Remove events that do not have the good duration.
-
-    Args:
-        idx_start: np.ndarray
-            Starting indices of event.
-
-        idx_stop: np.ndarray
-            Ending indices of event.
-
-        good_dur: np.ndarray
-            Indices of event having a proper duration.
-
-    Return:
-        good_idx: np.ndarray
-            Row vector containing the extending version of indices.
-    """
-    # Get where good duration start / end :
-    start = idx_start[good_dur]
-    stop = idx_stop[good_dur]
-
-    # Extend each spindle duration (start -> stop) :
-    extend = np.array([np.arange(i, j) for i, j in zip(start, stop)])
-
-    # Get it as a flatten array :
-    if extend.size:
-        return np.hstack(extend.flat)
-    else:
-        return np.array([], dtype=int)
-
-
-def _events_distance_fill(index, min_distance_ms, sf):
-    """Remove events that do not have the good duration.
-
-    Args:
-        index: np.ndarray
-            Indices of supra-threshold events.
-
-        min_distance_ms: int
-            Minimum distance (ms) between two events to consider them as two
-            distinct events
-
-        sf: int
-            Sampling frequency of the data (Hz)
-
-    Return:
-        f_index: np.ndarray
-            Filled (corrected) Indices of supra-threshold events
-    """
-    # Convert min_distance_ms
-    min_distance = min_distance_ms / 1000. * sf
-    idx_diff = np.diff(index)
-    condition = idx_diff > 1
-    idx_distance = np.where(condition)[0]
-    distance = idx_diff[condition]
-    bad = idx_distance[np.where(distance < min_distance)[0]]
-    # Fill gap between events separated with less than min_distance_ms
-    if len(bad) > 0:
-
-        fill = np.hstack([np.arange(index[j] + 1, index[j + 1])
-                          for i, j in enumerate(bad)])
-        f_index = np.sort(np.append(index, fill))
-        return f_index
-    else:
-        return index
-
-
-def _events_mean_freq(x, idx_start, idx_stop, sf):
-    """Remove events that do not have the good duration.
-
-    Args:
-        idx_start: np.ndarray
-            Starting indices of event.
-
-        idx_stop: np.ndarray
-            Ending indices of event.
-
-        sf: int
-            Sampling frequency of the data (Hz)
-
-    Return:
-        mfreq: np.ndarray
-            Mean frequency of each event (Hz)
-    """
-    analytic = hilbert(x) if x.size % 2 else hilbert(
-        x[:-1], len(x))
-    amplitude = np.abs(analytic)
-    phase = np.unwrap(np.angle(analytic))
-    inst_freq = abs(np.diff(phase) / (2.0 * np.pi) * sf)
-    mfreq = np.array([])
-    # Loop on each event
-    for i, j in zip(idx_start, idx_stop):
-        idx_event = np.array([np.arange(i, j)])
-        mfreq = np.append(mfreq, np.mean(inst_freq[idx_event]))
-
-    return mfreq
---
+++ visbrain/utils/sleep/event.py
@@ -0,0 +1,140 @@
+"""Goup of functions for index / event managment."""
+
+import numpy as np
+from scipy.signal import hilbert
+
+__all__ = ['_events_duration', '_events_removal', '_events_distance_fill',
+           '_events_mean_freq']
+
+
+def _events_duration(index, sf):
+    """Compute spindles/REM duration in ms.
+
+    Args:
+        index: np.ndarray
+            Index of events locations.
+
+        sf: float
+            The sampling frequency.
+
+    Returns:
+        number: int
+            Number of events
+
+        duration_ms: np.ndarray
+            Duration of each event
+
+        idx_start: np.ndarray
+            Array of integers where each event begin.
+
+        idx_end: np.ndarray
+            Array of integers where each event finish.
+    """
+    # Find boolean values where each spindle start :
+    bool_break = (index[1:] - index[:-1]) != 1
+    # Get spindles number :
+    number = bool_break.sum()
+    # Build starting / ending spindles index :
+    idx_start = np.hstack([np.array([0]), np.where(bool_break)[0] + 1])
+    idx_stop = np.hstack((idx_start[1::], len(index)))
+    # Compute duration :
+    duration_ms = np.diff(idx_start) * (1000. / sf)
+
+    return number, duration_ms, idx_start, idx_stop
+
+
+def _events_removal(idx_start, idx_stop, good_dur):
+    """Remove events that do not have the good duration.
+
+    Args:
+        idx_start: np.ndarray
+            Starting indices of event.
+
+        idx_stop: np.ndarray
+            Ending indices of event.
+
+        good_dur: np.ndarray
+            Indices of event having a proper duration.
+
+    Return:
+        good_idx: np.ndarray
+            Row vector containing the extending version of indices.
+    """
+    # Get where good duration start / end :
+    start = idx_start[good_dur]
+    stop = idx_stop[good_dur]
+
+    # Extend each spindle duration (start -> stop) :
+    extend = np.array([np.arange(i, j) for i, j in zip(start, stop)])
+
+    # Get it as a flatten array :
+    if extend.size:
+        return np.hstack(extend.flat)
+    else:
+        return np.array([], dtype=int)
+
+
+def _events_distance_fill(index, min_distance_ms, sf):
+    """Remove events that do not have the good duration.
+
+    Args:
+        index: np.ndarray
+            Indices of supra-threshold events.
+
+        min_distance_ms: int
+            Minimum distance (ms) between two events to consider them as two
+            distinct events
+
+        sf: float
+            Sampling frequency of the data (Hz)
+
+    Return:
+        f_index: np.ndarray
+            Filled (corrected) Indices of supra-threshold events
+    """
+    # Convert min_distance_ms
+    min_distance = min_distance_ms / 1000. * sf
+    idx_diff = np.diff(index)
+    condition = idx_diff > 1
+    idx_distance = np.where(condition)[0]
+    distance = idx_diff[condition]
+    bad = idx_distance[np.where(distance < min_distance)[0]]
+    # Fill gap between events separated with less than min_distance_ms
+    if len(bad) > 0:
+        fill = np.hstack([np.arange(index[j] + 1, index[j + 1])
+                          for i, j in enumerate(bad)])
+        f_index = np.sort(np.append(index, fill))
+        return f_index
+    else:
+        return index
+
+
+def _events_mean_freq(x, idx_start, idx_stop, sf):
+    """Remove events that do not have the good duration.
+
+    Args:
+        idx_start: np.ndarray
+            Starting indices of event.
+
+        idx_stop: np.ndarray
+            Ending indices of event.
+
+        sf: int
+            Sampling frequency of the data (Hz)
+
+    Return:
+        mfreq: np.ndarray
+            Mean frequency of each event (Hz)
+    """
+    analytic = hilbert(x) if x.size % 2 else hilbert(
+        x[:-1], len(x))
+    amplitude = np.abs(analytic)
+    phase = np.unwrap(np.angle(analytic))
+    inst_freq = abs(np.diff(phase) / (2.0 * np.pi) * sf)
+    mfreq = np.array([])
+    # Loop on each event
+    for i, j in zip(idx_start, idx_stop):
+        idx_event = np.array([np.arange(i, j)])
+        mfreq = np.append(mfreq, np.mean(inst_freq[idx_event]))
+
+    return mfreq
