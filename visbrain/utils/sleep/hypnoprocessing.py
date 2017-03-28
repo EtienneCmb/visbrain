545107f688d7e7bf2295d4cc0929c9aba897c7ef
EtienneCmb <e.combrisson@gmail.com>
2017-03-28 12:33:29 +0200

    Add original sampling frequency to stats info

--- visbrain/utils/sleep/hypnoprocessing.py
+++ visbrain/utils/sleep/hypnoprocessing.py
@@ -109,33 +109,33 @@ def sleepstats(file, hypno, N, sf=100., sfori=1000., time_window=30.):
     stats = {}
     tov = np.nan
 
-    stats['TDT_4'] = np.where(hypno != 0)[0].max() if np.nonzero(
+    stats['TDT_5'] = np.where(hypno != 0)[0].max() if np.nonzero(
         hypno)[0].size else tov
 
     # Duration of each sleep stages
-    stats['Art_5'] = hypno[hypno == -1].size
-    stats['W_6'] = hypno[hypno == 0].size
-    stats['N1_7'] = hypno[hypno == 1].size
-    stats['N2_8'] = hypno[hypno == 2].size
-    stats['N3_9'] = hypno[hypno == 3].size
-    stats['REM_10'] = hypno[hypno == 4].size
+    stats['Art_6'] = hypno[hypno == -1].size
+    stats['W_7'] = hypno[hypno == 0].size
+    stats['N1_8'] = hypno[hypno == 1].size
+    stats['N2_9'] = hypno[hypno == 2].size
+    stats['N3_10'] = hypno[hypno == 3].size
+    stats['REM_11'] = hypno[hypno == 4].size
 
     # Sleep stage latencies
-    stats['LatN1_11'] = np.where(hypno == 1)[0].min() if 1 in hypno else tov
-    stats['LatN2_12'] = np.where(hypno == 2)[0].min() if 2 in hypno else tov
-    stats['LatN3_13'] = np.where(hypno == 3)[0].min() if 3 in hypno else tov
-    stats['LatREM_14'] = np.where(hypno == 4)[0].min() if 4 in hypno else tov
+    stats['LatN1_12'] = np.where(hypno == 1)[0].min() if 1 in hypno else tov
+    stats['LatN2_13'] = np.where(hypno == 2)[0].min() if 2 in hypno else tov
+    stats['LatN3_14'] = np.where(hypno == 3)[0].min() if 3 in hypno else tov
+    stats['LatREM_15'] = np.where(hypno == 4)[0].min() if 4 in hypno else tov
 
-    if not np.isnan(stats['LatN1_11']) and not np.isnan(stats['TDT_4']):
-        hypno_s = hypno[stats['LatN1_11']:stats['TDT_4']]
+    if not np.isnan(stats['LatN1_12']) and not np.isnan(stats['TDT_5']):
+        hypno_s = hypno[stats['LatN1_12']:stats['TDT_5']]
 
-        stats['SPT_15'] = hypno_s.size
-        stats['WASO_16'] = hypno_s[hypno_s == 0].size
-        stats['TST_17'] = stats['SPT_15'] - stats['WASO_16']
+        stats['SPT_16'] = hypno_s.size
+        stats['WASO_17'] = hypno_s[hypno_s == 0].size
+        stats['TST_18'] = stats['SPT_16'] - stats['WASO_17']
     else:
-        stats['SPT_15'] = np.nan
-        stats['WASO_16'] = np.nan
-        stats['TST_17'] = np.nan
+        stats['SPT_16'] = np.nan
+        stats['WASO_17'] = np.nan
+        stats['TST_18'] = np.nan
 
     # Convert to minutes
     for key, value in stats.items():
@@ -143,11 +143,12 @@ def sleepstats(file, hypno, N, sf=100., sfori=1000., time_window=30.):
 
     # Add global informations
     stats['Filename_0'] = path.basename(file) if file is not None else ''
-    stats['Downsampling_1'] = str(int(sf)) + " Hz"
-    stats['Units_2'] = 'minutes'
-    stats['Duration (TIB)_3'] = N / (sfori * 60.)
+    stats['Sampling frequency_1'] = str(int(sfori)) + " Hz"
+    stats['Down-sampling_2'] = str(int(sf)) + " Hz"
+    stats['Units_3'] = 'minutes'
+    stats['Duration (TIB)_4'] = N / (sfori * 60.)
 
-    stats['SE (%)_18'] = np.round(stats['TST_17'] / stats['TDT_4'] * 100., 2)
+    stats['SE (%)_19'] = np.round(stats['TST_18'] / stats['TDT_5'] * 100., 2)
 
     # Percentages of TDT
     # stats['%Art_18'] = stats['Art_4'] / stats['TDT_3'] * 100.
