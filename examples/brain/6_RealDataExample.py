2f48ac8f8778820a8bda66ba5c0c80f2df94e557
EtienneCmb <e.combrisson@gmail.com>
2017-04-02 19:04:39 +0200

    Reset examples

--- examples/brain/6_RealDataExample.py
+++ examples/brain/6_RealDataExample.py
@@ -10,6 +10,4 @@ s_xyz = data['xyz']
 
 vb = Brain(s_xyz=s_xyz, s_data=s_data, s_cmap_vmin=-1, s_cmap_vmax=1,
            s_cmap='jet', s_cmap_clim=(-1, 1))
-vb._cortProj()
-vb.light_reflection('external')
 vb.show()
