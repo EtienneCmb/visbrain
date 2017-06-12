6ba1ea75a892f104b8ed0fe2ccf885e3072bbb2a
EtienneCmb <e.combrisson@gmail.com>
2017-06-12 15:57:44 +0200

    Format imports

--- test/test_imports.py
+++ test/test_imports.py
@@ -2,3 +2,19 @@ from visbrain import Brain
 from visbrain import Sleep
 from visbrain import Ndviz
 from visbrain import Figure
+
+def test_import_Brain():
+    """Import the Brain module."""
+    from visbrain import Brain
+
+def test_import_Sleep():
+    """Import the Sleep module."""
+    from visbrain import Sleep
+
+def test_import_Ndviz():
+    """Import the Ndviz module."""
+    from visbrain import Ndviz
+
+def test_import_Figure():
+    """Import the Figure module."""
+    from visbrain import Figure
