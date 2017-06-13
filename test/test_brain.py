498110682c93f827451083c6a95fc52c7f9da7e1
EtienneCmb <e.combrisson@gmail.com>
2017-06-13 08:28:20 +0200

    Add tests for required Sleep files

--- test/test_brain.py
+++ test/test_brain.py
@@ -1,16 +1,16 @@
-import visbrain
 from visbrain import Brain
 import os
+from distutils.sysconfig import get_python_lib
 
 
 def test_instance_brain():
-    """Test the creation of a Brain instance"""
+    """Test the creation of a Brain instance."""
     pass
     # Brain()
 
 
 def test_brain_templates():
     """Test is templates are installed."""
-    vbpath = visbrain.__file__.split('__init__')[0] + 'brain/base/templates/'
+    vbpath = get_python_lib() + '/visbrain/brain/base/templates/'
     for k in ['B1.npz', 'B2.npz', 'B3.npz', 'roi.npz']:
         assert os.path.isfile(os.path.join(vbpath, k))
--- test/test_sleep.py
+++ test/test_sleep.py
@@ -1,6 +1,21 @@
 from visbrain import Sleep
+import os
+from distutils.sysconfig import get_python_lib
+
 
 def test_instance_sleep():
     """Test the creation of a Sleep instance."""
     pass
     # s = Sleep()
+
+
+def test_topo_file():
+    """Test if the topo reference file is installed."""
+    vbpath = get_python_lib() + '/visbrain/utils/topo/'
+    assert os.path.isfile(os.path.join(vbpath, 'eegref.npz'))
+
+
+def test_ico_file():
+    """Test if Sleep icon is installed."""
+    vbpath = get_python_lib() + '/visbrain/sleep/ico/'
+    assert os.path.isfile(os.path.join(vbpath, 'sleep.svg'))
