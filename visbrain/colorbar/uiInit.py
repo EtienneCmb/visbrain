aca7c7cab35f95fd1d847990f6ffe63469378317
EtienneCmb <e.combrisson@gmail.com>
2017-06-21 20:47:42 +0200

    Configura GUI creation

---
+++ visbrain/colorbar/uiInit.py
@@ -0,0 +1,16 @@
+from PyQt5 import QtWidgets
+from vispy import app, scene
+
+from .gui import Ui_MainWindow
+
+__all__ = ['uiInit']
+
+
+class uiInit(QtWidgets.QMainWindow, Ui_MainWindow, app.Canvas):
+    """Group and initialize the graphical elements and interactions."""
+
+    def __init__(self):
+        """Init."""
+        # Create the main window :
+        super(uiInit, self).__init__(None)
+        self.setupUi(self)
