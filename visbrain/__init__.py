8cf6816ce052e13167f284155fe88546c772ec10
EtienneCmb <e.combrisson@gmail.com>
2017-06-22 10:35:17 +0200

    Avoid PyQt5 crash on error. See https://stackoverflow.com/questions/38020020/pyqt5-app-exits-on-error-where-pyqt4-app-would-not

--- visbrain/__init__.py
+++ visbrain/__init__.py
@@ -16,9 +16,20 @@ Right now, visbrain contains five modules :
 
 See etiennecmb.github.io/visbrain for a complete and step-by step documentation
 """
-
+# Import modules :
 from .brain import Brain
 from .ndviz import Ndviz
 from .sleep import Sleep
 from .figure import Figure
 from .colorbar import Colorbar
+
+# PyQt5 crash if an error occured. This small function fix it for all modules
+# to retrieve the PyQt4 behavior :
+import sys
+
+
+def PyQt4_behavior(type, value, tback):
+    """Retrieve PyQt4 behavior if an error occured."""
+    sys.__excepthook__(type, value, tback)
+
+sys.excepthook = PyQt4_behavior
