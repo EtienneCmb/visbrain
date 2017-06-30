7653539f32db9863f9a2f95801c3fed88ed0174d
EtienneCmb <e.combrisson@gmail.com>
2017-06-30 10:36:25 +0200

    Move ViewBase -> uiInit

--- visbrain/brain/interface/uiInit.py
+++ visbrain/brain/interface/uiInit.py
@@ -1,14 +1,18 @@
-"""Define some basic viewing elements (canvas / shortcuts).
+"""This script group the diffrent graphical components.
 
-This file contain:
-    * vbShortcuts : main class for shortcuts managment
-    * vbCanvas : create Brain main canvas and the canvas
-    for the colorbar
+Grouped components :
+    * PyQt elements (window, Pyqt functions...)
+    * Vispy canvas functions
+    * User shortcuts
 """
 
-from vispy import scene
+from PyQt5 import QtWidgets
+from vispy import app, scene
 
-__all__ = ['vbCanvas', 'vbShortcuts']
+from .gui import Ui_MainWindow
+from ...utils import color2vb
+
+__all__ = ['uiInit']
 
 
 class vbShortcuts(object):
@@ -128,23 +132,50 @@ class vbCanvas(object):
     def __init__(self, bgcolor=(0, 0, 0)):
         """Init."""
         # Initialize main canvas:
-        self.canvas = scene.SceneCanvas(keys='interactive', show=False, dpi=600,
-                                        bgcolor=bgcolor, fullscreen=True,
-                                        resizable=True, position=(0, 250))
+        self.canvas = scene.SceneCanvas(keys='interactive', show=False,
+                                        dpi=600, bgcolor=bgcolor,
+                                        fullscreen=True, resizable=True)
         self.wc = self.canvas.central_widget.add_view()
-
-        # Initialize colorbar canvas :
-        self.cbcanvas = scene.SceneCanvas(keys='interactive', bgcolor=bgcolor,
-                                          resizable=True, )
-        self.cbwc = self.cbcanvas.central_widget.add_view()
-
-        # Add axis (debugging):
-        # self._axis = scene.visuals.XYZAxis()
-        # self.wc.add(self._axis)
-
         # Visualization settings. The min/maxOpacity attributes are defined
         # because it seems that OpenGL have trouble with small opacity (usually
         # between 0 and 1). Defining min and max far away from 0 / 1 solve this
         # problem.
         self.minOpacity = -10000
         self.maxOpacity = 10000
+
+
+class uiInit(QtWidgets.QMainWindow, Ui_MainWindow, app.Canvas, vbShortcuts):
+    """Group and initialize the graphical elements and interactions.
+
+    Kargs:
+        bgcolor: tuple, optional, (def: (0.1, 0.1, 0.1))
+            Background color of the main window. The same background
+            will be used for the colorbar panel so that future figures
+            can be uniform.
+    """
+
+    def __init__(self, bgcolor=(0.1, 0.1, 0.1)):
+        """Init."""
+        # Create the main window :
+        super(uiInit, self).__init__(None)
+        self.setupUi(self)
+        if self._savename is not None:
+            self.setWindowTitle('Brain - '+self._savename)
+
+        # Initlialize view :
+        self.view = vbCanvas(bgcolor)
+
+        # Add the canvas to the UI (vBrain widget) and colorbar:
+        self.vBrain.addWidget(self.view.canvas.native)
+
+        # Set background color and hide quick settings panel :
+        self.bgcolor = tuple(color2vb(color=bgcolor, length=1)[0, 0:3])
+        self.q_widget.hide()
+
+        # Set background elements :
+        self.bgd_red.setValue(self.bgcolor[0])
+        self.bgd_green.setValue(self.bgcolor[1])
+        self.bgd_blue.setValue(self.bgcolor[2])
+
+        # Initialize shortcuts :
+        vbShortcuts.__init__(self, self.view.canvas)
