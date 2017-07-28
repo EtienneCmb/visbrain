3730824cbbb46459454573c7d678b3c117a6303f
EtienneCmb <e.combrisson@gmail.com>
2017-07-28 17:29:43 +0200

    Cbar flake8

--- visbrain/utils/cbar/CbarBase.py
+++ visbrain/utils/cbar/CbarBase.py
@@ -1,11 +1,11 @@
-
+"""Most basic colorbar class."""
 from ..color import color2tuple
 
 __all__ = ['CbarArgs', 'CbarBase']
 
 
 class CbarArgs(object):
-    """Manage the diffrent inputs for the colormap creation.
+    """Manage the different inputs for the colormap creation.
 
     Parameters
     ----------
@@ -122,17 +122,20 @@ class CbarBase(CbarArgs):
         self._ndigits = ndigits
         self._width = width
         if fcn is None:
-            def fcn(): pass
+            def fcn():
+                pass
         self._fcn = fcn
         if minmaxfcn is None:
-            def minmaxfcn(): pass
+            def minmaxfcn():
+                pass
         self._minmaxfcn = minmaxfcn
 
     def __getitem__(self, key):
         """Get item (usefull for CbarObjects)."""
-        return eval('self._'+key)
+        return eval('self._' + key)
 
     def __setitem__(self, key, value):
+        """Set self items."""
         if not isinstance(value, str):
             exec("self._" + key + "=" + str(value))
         else:
