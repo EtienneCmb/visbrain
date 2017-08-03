0aaee17a418ca628bcf2560455cd2655b840c272
EtienneCmb <e.combrisson@gmail.com>
2017-08-04 01:30:44 +0200

    Start functions for finding ROI for sources

--- visbrain/utils/physio.py
+++ visbrain/utils/physio.py
@@ -1,12 +1,15 @@
 """Group of functions for physiological processing."""
+
 import numpy as np
 from re import findall
+import os
+import sys
 
-__all__ = ('find_nonEEG', 'rereferencing', 'bipolarization', 'commonaverage',
-           'tal2mni')
+__all__ = ('find_non_eeg', 'rereferencing', 'bipolarization', 'commonaverage',
+           'tal2mni', 'mni2tal', 'find_roi')
 
 
-def find_nonEEG(channels, pattern=['eog', 'emg', 'ecg', 'abd']):
+def find_non_eeg(channels, pattern=['eog', 'emg', 'ecg', 'abd']):
     """Find non-EEG channels.
 
     Parameters
@@ -272,6 +275,37 @@ def tal2mni(xyz):
     Parameters
     ----------
     xyz : array_like
+        Array of Talairach coordinates of shape (n_sources, 3)
+
+    Returns
+    -------
+    xyz_r : array_like
+        Array of MNI coordinates of shape (n_sources, 3)
+    """
+    # Check xyz to be (n_sources, 3) :
+    if (xyz.ndim != 2) or (xyz.shape[1] != 3):
+        raise ValueError("The shape of xyz must be (N, 3).")
+    n_sources = xyz.shape[0]
+
+    # Transformation matrices, different zooms above/below AC :
+    rotn = np.linalg.inv(_spm_matrix([0., 0., 0., .05]))
+    upz = np.linalg.inv(_spm_matrix([0., 0., 0., 0., 0., 0., .99, .97, .92]))
+    downz = np.linalg.inv(_spm_matrix([0., 0., 0., 0., 0., 0., .99, .97, .84]))
+
+    # Apply rotation and translation :
+    xyz = rotn * np.c_[xyz, np.ones((n_sources, ))].T
+    tmp = np.array(xyz)[2, :] < 0.
+    xyz[:, tmp] = downz * xyz[:, tmp]
+    xyz[:, ~tmp] = upz * xyz[:, ~tmp]
+    return np.array(xyz[0:3, :].T)
+
+
+def mni2tal(xyz):
+    """Transform MNI coordinates into Talairach.
+
+    Parameters
+    ----------
+    xyz : array_like
         Array of MNI coordinates of shape (n_sources, 3)
 
     Returns
@@ -284,14 +318,65 @@ def tal2mni(xyz):
         raise ValueError("The shape of xyz must be (N, 3).")
     n_sources = xyz.shape[0]
 
-    # Transformation matrices, different zooms above/below AC
-    rotn = np.linalg.inv(_spm_matrix([0., 0., 0., .05]))
-    upz = np.linalg.inv(_spm_matrix([0., 0., 0., 0., 0., 0., .99, .97, .92]))
-    downz = np.linalg.inv(_spm_matrix([0., 0., 0., 0., 0., 0., .99, .97, .84]))
+    # Transformation matrices, different zooms above/below AC :
+    up_t = _spm_matrix([0., 0., 0., .05, 0., 0., .99, .97, .92])
+    down_t = _spm_matrix([0., 0., 0., .05, 0., 0., .99, .97, .84])
+    xyz = np.c_[xyz, np.ones((n_sources, ))].T
 
-    # Apply rotation :
-    xyz = rotn * np.c_[xyz, np.ones((n_sources, ))].T
     tmp = np.array(xyz)[2, :] < 0.
-    xyz[:, tmp] = downz * xyz[:, tmp]
-    xyz[:, ~tmp] = upz * xyz[:, ~tmp]
-    return xyz[0:3, :].T
+    xyz[:, tmp] = down_t * xyz[:, tmp]
+    xyz[:, ~tmp] = up_t * xyz[:, ~tmp]
+    return np.array(xyz[0:3, :].T)
+
+def find_roi(xyz, r=5., nearest=True):
+    # Check xyz to be (n_sources, 3) :
+    if (xyz.ndim != 2) or (xyz.shape[1] != 3):
+        raise ValueError("The shape of xyz must be (N, 3).")
+    # xyz = tal2mni(xyz)
+    n_sources = xyz.shape[0]
+    xyz = np.c_[xyz, np.ones((n_sources,))].T
+
+    # Load ROI atlas :
+    pathfile = sys.modules[__name__].__file__.split('utils')[0]
+    path_to_template = os.path.join(*('brain', 'base', 'templates', 'roi.npz'))
+    path = os.path.join(pathfile, path_to_template)
+    atlas = np.load(path)
+
+    # Get volumes :
+    aal_label, aal_idx = atlas['aal_label'], atlas['aal_idx']
+    vol, hdr, brod_idx = atlas['vol'], atlas['hdr'], atlas['brod_idx']
+    sh = vol.shape
+
+    # Build AAL label for left and right :
+    left = np.full((len(aal_label),), ' (L)')
+    right = np.full((len(aal_label),), ' (R)')
+    aal_l = np.core.defchararray.add(aal_label, left)
+    aal_r = np.core.defchararray.add(aal_label, right)
+    aal_label = np.c_[aal_l, aal_r].flatten()
+    # print(aal_label.shape, aal_idx.shape, 'AAl : ', aal_idx.min(), aal_idx.max(), 'VOL : ', vol.min(), vol.max())
+
+    # Loop over source :
+    info = np.full((n_sources, 5), '-', dtype=object)
+    for k in range(n_sources):
+        info[k, 2::] = xyz[0:-1, k].ravel()
+        # Apply HDR transformation :
+        pos = np.linalg.lstsq(hdr, xyz[:, [k]])[0].reshape(-1)
+        sub = list(np.around(pos).astype(int))
+
+        if (sub[0] <= sh[0]) and (sub[1] <= sh[1]) and (sub[2] <= sh[2]):
+
+            if nearest:
+                pass
+            else:
+                # Find the Brodmann area :
+                brod = brod_idx[sub[0], sub[1], sub[2]]
+                if brod:
+                    info[k, 0] = 'Brodmann area ' + str(brod)
+                # Find the AAL :
+                aal = vol[sub[0], sub[1], sub[2]]
+                if aal:
+                    info[k, 1] = aal_label[aal - 1]
+        else:
+            print('BAD')
+
+    print(info)
