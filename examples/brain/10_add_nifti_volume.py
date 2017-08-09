140caac53a80647c5e9b06d83d25d051b54afdf4
EtienneCmb <e.combrisson@gmail.com>
2017-08-09 16:33:03 +0200

    Add cross-section to the nifti example

--- examples/brain/10_add_nifti_volume.py
+++ examples/brain/10_add_nifti_volume.py
@@ -8,13 +8,24 @@ downloaded.
 When the volume is loaded into the GUI, it's accessible from the
 Brain/Cross-sections and Brain/Volume tab.
 """
+from __future__ import print_function
+import numpy as np
 import os
 
 from visbrain import Brain
 from visbrain.io import read_nifti
 
+# Define four sources sources :
+s_xyz = np.array([[29.9, -37.3, -19.3],
+                  [-5.33, 14.00, 20.00],
+                  [25.99, 14.00, 34.66],
+                  [0., -1.99, 10.66]])
+
 # Define a Brain instance :
-vb = Brain()
+vb = Brain(s_xyz=s_xyz)
+
+# Print the list of volumes avaible :
+print('Volumes avaible by default : ', vb.volume_list())
 
 # If you don't have access to a Nifti file, download one of the volume avaible
 # at https://brainder.org/download/flair/.
@@ -29,6 +40,7 @@ data1, header1, tf1 = read_nifti(path1)
 
 # Add the volume to the GUI :
 vb.add_volume('Volume1', data1, transform=tf1)
+print('Add Volume1 to the list of volumes : ', vb.volume_list())
 
 # You can add multiple volumes :
 path_to_nifti2 = os.getenv("HOME")
@@ -36,5 +48,10 @@ file_nifti2 = 'GG-853-WM-0.7mm.nii.gz'
 path2 = os.path.join(path_to_nifti2, file_nifti2)
 data2, header2, tf2 = read_nifti(path2)
 vb.add_volume('Volume2', data2, transform=tf2)
+print('Add Volume2 to the list of volumes : ', vb.volume_list())
+
+# Set the cross-section to be centered on the last source :
+vb.set_cross_sections(pos=s_xyz[3, :], volume='Volume2', cmap='gist_stern',
+                      split_view=True)
 
 vb.show()
