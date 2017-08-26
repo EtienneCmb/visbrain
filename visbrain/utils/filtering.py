ead8bbe96c4279f6582ad792ff9a04c39102bda3
EtienneCmb <e.combrisson@gmail.com>
2017-08-26 11:38:27 +0200

    filtering pep8 and flake8

--- visbrain/utils/filtering.py
+++ visbrain/utils/filtering.py
@@ -3,7 +3,7 @@
 import numpy as np
 from scipy.signal import butter, filtfilt, lfilter, bessel, welch
 
-__all__ = ['filt', 'morlet', 'ndmorlet', 'morlet_power', 'welch_power']
+__all__ = ('filt', 'morlet', 'ndmorlet', 'morlet_power', 'welch_power')
 
 #############################################################################
 # FILTERING
@@ -14,38 +14,31 @@ def filt(sf, f, x, btype='bandpass', order=3, method='butterworth',
          way='filtfilt', axis=0):
     """Filt data.
 
-    Args:
-        sf: float
-            The sampling frequency
+    Parameters
+    ----------
+    sf : float
+        The sampling frequency
+    f : array_like
+        Frequency vector (2,)
+    x : array_like
+        The data to filt.
+    btype : {'bandpass', 'bandstop', 'highpass', 'lowpass'}
+        If highpass, the first value of f will be used. If lowpass
+        the second value of f will be used.
+    order : int | 3
+        The filter order.
+    method : {'butterworth', 'bessel'}
+        Filter type to use.
+    way : {'filtfilt', 'lfilter'}
+        Specify if the filter has to be one way ('lfilter') or two ways
+        ('filtfilt').
+    axis : int | 0
+        The axis along which the filter is applied.
 
-        f: np.ndarray
-            Frequency vector (2,)
-
-        x: np.ndarray
-            The data to filt.
-
-    Kargs:
-        btype: string, optional, (def: 'bandpass')
-            The filter type. Choose between bandpass, bandstop, highpass,
-            lowpass. If highpass, the first value of f will be used. If lowpass
-            the second value of f will be used.
-
-        order: int, optional, (def: 3)
-            The filter order.
-
-        method: string, optional, (def: 'butterworth')
-            The type of filter to use. Could be butterworth or bessel.
-
-        way: string, optional, (def: 'filtfilt')
-            Specify if the filter has to be one way ('lfilter') or two ways
-            ('filtfilt').
-
-        axis: int, optional, (def: 0)
-            The axis along which the filter is applied.
-
-    Returns:
-        xfilt: np.ndarray
-            Filtered data.
+    Returns
+    -------
+    xfilt : array_like
+        Filtered data.
     """
     # Normalize frequency vector according to btype :
     if btype in ['bandpass', 'bandstop']:
@@ -75,19 +68,16 @@ def filt(sf, f, x, btype='bandpass', order=3, method='butterworth',
 def _morlet_wlt(sf, f, width=7.0):
     """Get a Morlet's wavelet.
 
-    Args:
-        sf: float
-            Sampling frequency
-
-        f: np.ndarray, shape (2,)
-            Frequency vector
-
-        width: float, optional, (def: 7.0)
-            Width of the wavelet
-
-    Kargs:
-        wlt: np.ndarray
-            Morlet wavelet
+    Parameters
+    ----------
+    sf : float
+        Sampling frequency.
+    f : array_like
+        Frequency vector of shape (2,).
+    width : float | 7.0
+        Width of the wavelet.
+    wlt: array_like
+        Morlet wavelet.
     """
     sf, f, width = float(sf), float(f), float(width)
     dt = 1 / sf
@@ -96,9 +86,9 @@ def _morlet_wlt(sf, f, width=7.0):
 
     # Build morlet wavelet :
     t = np.arange(-width * st / 2, width * st / 2, dt)
-    A = 1 / np.sqrt((st * np.sqrt(np.pi)))
-    wlt = A * np.exp(-np.square(t) / (2 * np.square(st))) * np.exp(
-                                                       1j * 2 * np.pi * f * t)
+    a = 1 / np.sqrt((st * np.sqrt(np.pi)))
+    wlt = a * np.exp(-np.square(t) / (2 * np.square(st))) * np.exp(
+        1j * 2 * np.pi * f * t)
 
     return wlt
 
@@ -106,24 +96,22 @@ def _morlet_wlt(sf, f, width=7.0):
 def morlet(x, sf, f, width=7.0):
     """Complex decomposition of a signal x using the morlet wavelet.
 
-    Args:
-        x: np.ndarray, shape (N,)
-            The signal to use for the complex decomposition. Must be
-            a vector of length N.
+    Parameters
+    ----------
+    x : array_like
+        The signal to use for the complex decomposition. Must be
+        a vector of length N.
+    sf : float
+        Sampling frequency
+    f : array_like, shape (2,)
+        Frequency vector
+    width : float | 7.0
+        Width of the wavelet
 
-        sf: float
-            Sampling frequency
-
-        f: np.ndarray, shape (2,)
-            Frequency vector
-
-    Kargs:
-        width: float, optional, (def: 7.0)
-            Width of the wavelet
-
-    Returns:
-        xout: np.ndarray, shape (N,)
-            The complex decomposition of the signal x.
+    Returns
+    -------
+    xout: array_like
+        The complex decomposition of the signal x.
     """
     # Get the wavelet :
     m = _morlet_wlt(sf, f, width)
@@ -138,23 +126,21 @@ def morlet(x, sf, f, width=7.0):
 def ndmorlet(x, sf, f, axis=0, get=None, width=7.0):
     """Complex decomposition using Morlet's wlt for a multi-dimentional array.
 
-    Args:
-    x: array
+    Parameters
+    ----------
+    x : array_like
         The signal to use for the complex decomposition.
-
-    sf: float
+    sf : float
         Sampling frequency
-
-    f: array, shape (2,)
-        Frequency vector
-
-    axis: integer, optional, (def: 0)
+    f : array_like
+        Frequency vector of shape (2,)
+    axis : integer | 0
         Specify the axis where is located the time dimension
-
-    width: float, optional, (def: 7.0)
+    width : float | 7.0
         Width of the wavelet
 
-    Returns:
+    Returns
+    -------
         xout: array, same shape as x
             Complex decomposition of x.
     """
@@ -162,13 +148,13 @@ def ndmorlet(x, sf, f, axis=0, get=None, width=7.0):
     m = _morlet_wlt(sf, f, width)
 
     # Define a morlet function :
-    def morletFcn(xt):
+    def morlet_fcn(xt):
         # Compute morlet :
         y = np.convolve(xt, m)
         return y[int(np.ceil(len(m) / 2)) - 1:int(len(y) - np.floor(
-                                                                len(m) / 2))]
+            len(m) / 2))]
 
-    xf = np.apply_along_axis(morletFcn, axis, x)
+    xf = np.apply_along_axis(morlet_fcn, axis, x)
     # Get amplitude / power / phase :
     if get == 'amplitude':
         return np.abs(xf)
@@ -181,26 +167,24 @@ def ndmorlet(x, sf, f, axis=0, get=None, width=7.0):
 def morlet_power(x, freqs, sf, norm=True):
     """Compute bandwise-normalized power of data using morlet wavelet.
 
-    Args:
-        x: np.ndarray
-            Row vector signal.
+    Parameters
+    ----------
+    x : array_like
+        Row vector signal.
+    freqs : array_like
+        Frequency bands for power computation. The power will be computed
+        using successive frequency band (e.g freqs=(1., 2, .3)).
+    sf : float
+        Sampling frequency.
+    norm : bool | True
+        If True, return bandwise normalized band power
+        (For each time point, the sum of power in the 4 band equals 1)
 
-        freqs: np.array
-            Frequency bands for power computation. The power will be computed
-            using successive frequency band (e.g freqs=(1., 2, .3)).
-
-        sf: float
-            Sampling frequency.
-
-    Kargs:
-        norm: boolean, optional (def True)
-            If True, return bandwise normalized band power
-            (For each time point, the sum of power in the 4 band equals 1)
-
-    Returns:
-        xpow: np.ndarray
-            The power in the specified frequency bands of shape
-            (len(freqs)-1, npts).
+    Returns
+    -------
+    xpow : array_like
+        The power in the specified frequency bands of shape
+        (len(freqs)-1, npts).
     """
     # Build frequency vector :
     f = np.c_[freqs[0:-1], freqs[1::]].mean(1)
@@ -217,29 +201,25 @@ def morlet_power(x, freqs, sf, norm=True):
     return xpow
 
 
-def welch_power(x, fMin, fMax, sf, window_s=30, norm=True):
+def welch_power(x, fmin, fmax, sf, window_s=30, norm=True):
     """Compute bandwise-normalized power of data using welch.
 
-    Args:
-        x: np.ndarray
-            Signal
-
-        sf: int
-            Downsampling frequency
-
-    Kargs:
-        window_s: int, optional (def 30)
-            Time resolution (sec) of Welch's periodogram. Must be > 10 seconds.
-
-        norm: boolean, optional (def True)
-            If True, return normalized band power
-
+    Parameters
+    ----------
+    x : array_like
+        Signal
+    sf : int
+        Downsampling frequency
+    window_s : int, optional (def 30)
+        Time resolution (sec) of Welch's periodogram. Must be > 10 seconds.
+    norm : boolean, optional (def True)
+        If True, return normalized band power
     """
     sf = int(sf)
     freq_spacing = 0.1
     f_vector = np.arange(0, sf / 2 + freq_spacing, freq_spacing)
-    idx_fMin = np.where(f_vector == fMin)[0][0]
-    idx_fMax = np.where(f_vector == fMax)[0][0] + 1
+    idx_fmin = np.where(f_vector == fmin)[0][0]
+    idx_fmax = np.where(f_vector == fmax)[0][0] + 1
     delta_spec = np.array([], dtype=float)
     delta_spec_norm = np.array([], dtype=float)
 
@@ -247,8 +227,8 @@ def welch_power(x, fMin, fMax, sf, window_s=30, norm=True):
         f, Pxx_spec = welch(x[i:i + window_s * sf], sf, 'hanning',
                             nperseg=sf * (1 / freq_spacing),
                             scaling='spectrum')
-        mean_delta = np.mean(Pxx_spec[idx_fMin:idx_fMax, ])
-        norm_delta = np.sum(Pxx_spec[idx_fMin:idx_fMax, ]) / np.sum(Pxx_spec)
+        mean_delta = np.mean(Pxx_spec[idx_fmin:idx_fmax, ])
+        norm_delta = np.sum(Pxx_spec[idx_fmin:idx_fmax, ]) / np.sum(Pxx_spec)
         delta_spec = np.append(delta_spec, mean_delta)
         delta_spec_norm = np.append(delta_spec_norm, norm_delta)
 
