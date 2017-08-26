072607d141679a11900b201bb1f52390f0f3afe1
EtienneCmb <e.combrisson@gmail.com>
2017-08-26 15:22:47 +0200

    Update annotation marker

--- visbrain/sleep/interface/uiElements/uiAnnotate.py
+++ visbrain/sleep/interface/uiElements/uiAnnotate.py
@@ -1,6 +1,7 @@
 """Enable to annotate a Sleep file."""
 
 from PyQt5 import QtWidgets
+import numpy as np
 
 __all__ = ['uiAnnotate']
 
@@ -26,22 +27,28 @@ class uiAnnotate(object):
             val = self._SlVal.value()
             step = self._SigSlStep.value()
             win = self._SigWin.value()
-            xlim = (val*step, val*step+win)
+            xlim = (val * step, val * step + win)
         # Automatically set the window :
         self._AnnotateTable.setItem(rw, 0, QtWidgets.QTableWidgetItem(
-                                                              str(xlim[0])))
+            str(xlim[0])))
         self._AnnotateTable.setItem(rw, 1, QtWidgets.QTableWidgetItem(
-                                                              str(xlim[1])))
+            str(xlim[1])))
         # Select the text item :
         self._AnnotateTable.setCurrentCell(rw, 2)
         item = QtWidgets.QTableWidgetItem()
         item.setText(txt)
         self._AnnotateTable.setItem(rw, 2, item)
         self._AnnotateTable.editItem(self._AnnotateTable.item(rw, 2))
+        # Send marker annotation to the time-axis :
+        self._annot_mark = np.append(self._annot_mark, np.array(xlim).mean())
+        self._fcn_sliderMove()
 
     def _fcn_annotateRm(self):
         """Remove a line to the annotation table."""
-        self._AnnotateTable.removeRow(self._AnnotateTable.currentRow())
+        row = self._AnnotateTable.currentRow()
+        self._AnnotateTable.removeRow(row)
+        self._annot_mark = np.delete(self._annot_mark, row, 0)
+        self._fcn_sliderMove()
 
     def _fcn_annotateGoto(self):
         """Go to the annotation location."""
