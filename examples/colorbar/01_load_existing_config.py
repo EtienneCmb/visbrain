100fdd0488e5e4acd071d91ab0a23ac4f8855272
EtienneCmb <e.combrisson@gmail.com>
2017-08-30 11:34:28 +0200

    Sharable link for colorbar examples

--- examples/colorbar/01_load_existing_config.py
+++ examples/colorbar/01_load_existing_config.py
@@ -5,6 +5,11 @@ Load an existing colorbar configuration file
 The configuration is in a config.txt file and is loaded to reproduce the
 colorbar configuration.
 
+Configuration file 1 (config_1.txt) :
+https://drive.google.com/open?id=0B6vtJiCQZUBvREFEX08zSWRFbzQ
+Configuration file 2 (config_2.txt) :
+https://drive.google.com/open?id=0B6vtJiCQZUBvWVM3U3RrSWxkeE0
+
 .. image:: ../../picture/piccbar/ex_load_existing_config.png
 """
 from visbrain import Colorbar
