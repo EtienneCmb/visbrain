c21ff30240a6132eba4e449b53929c36c1eda0ac
EtienneCmb <e.combrisson@gmail.com>
2017-09-12 17:21:36 +0200

    flake8

--- visbrain/io/rw_utils.py
+++ visbrain/io/rw_utils.py
@@ -5,22 +5,23 @@
 """
 import os
 
-__all__ = ['get_file_ext', 'safety_save']
+__all__ = ('get_file_ext', 'safety_save')
 
 
 def get_file_ext(path):
     """Get the filename and extension.
 
-    Args:
-        path: strin
-            Path to the file.
+    Parameters
+    ----------
+    path : string
+        Path to the file.
 
-    Returns:
-        file: string
-            Name of the file
-
-        ext: string
-            Extension of the file.
+    Returns
+    -------
+    file : string
+        Name of the file
+    ext : string
+        Extension of the file.
     """
     # Test if file exist :
     assert os.path.isfile(path)
@@ -32,26 +33,26 @@ def get_file_ext(path):
 
 
 def safety_save(path, limit=100):
-    """If a file exist, avoid arasing it when saving.
+    """If a file exist, avoid erasing it when saving.
 
-    Args:
-        path: string
-            Path to the file.
+    Parameters
+    ----------
+    path : string
+        Path to the file.
+    limit : int | 100
+        Limit for the filename occurence.
 
-    Kargs:
-        limit: int, optional, (def: 100)
-            Limit for the filename occurence.
-
-    Returns:
-        name: string
-            Unique filename.
+    Returns
+    -------
+    name : string
+        Unique filename.
     """
     k = 1
     while os.path.isfile(path) and (k < limit):
         fname, fext = os.path.splitext(path)
-        if fname.find('(')+1:
-            path = fname[0:fname.find('(')+1]+str(k)+')'+fext
+        if fname.find('(') + 1:
+            path = fname[0:fname.find('(') + 1] + str(k) + ')' + fext
         else:
-            path = fname+'('+str(k)+')'+fext
+            path = fname + '(' + str(k) + ')' + fext
         k += 1
     return path
