180ad8c535c15d3d79199a9816fc907b7dd9b710
EtienneCmb <e.combrisson@gmail.com>
2017-09-16 17:13:48 +0200

    Annotations support

---
+++ visbrain/gridsignals/ui_elements/ui_annotations.py
@@ -0,0 +1,60 @@
+"""Signal annotations."""
+from PyQt5 import QtWidgets
+
+__all__ = ('UiAnnotations')
+
+
+class UiAnnotations(object):
+    """Annotations interactions."""
+
+    def __init__(self):
+        """Init."""
+        self._annot_table.itemSelectionChanged.connect(self._fcn_annot_goto)
+        # Table edited :
+        self._annot_table.cellChanged.connect(self._fcn_text_edited)
+
+    def _annotate_event(self, signal, coord, text="Enter your annotation"):
+        """Annotate event."""
+        self._annot_table.setRowCount(self._annot_table.rowCount() + 1)
+        rw = self._annot_table.rowCount() - 1
+        # Add annotation :
+        self._annot_table.setItem(rw, 0, QtWidgets.QTableWidgetItem(
+            str(coord)))
+        self._annot_table.setItem(rw, 1, QtWidgets.QTableWidgetItem(
+            str(signal)))
+        self._annot_table.setItem(rw, 2, QtWidgets.QTableWidgetItem(
+            str(text)))
+        # Select the text item :
+        self._annot_table.setCurrentCell(rw, 2)
+        item = QtWidgets.QTableWidgetItem()
+        item.setText(text)
+        self._annot_table.setItem(rw, 2, item)
+        self._annot_table.editItem(self._annot_table.item(rw, 2))
+
+    def _fcn_annot_goto(self):
+        """Select the annotation."""
+        row = self._annot_table.currentRow()
+        if row >= 0:
+            # Get coordinates and the signal :
+            coord = self._annot_table.item(row, 0).text()
+            signal = self._annot_table.item(row, 1).text()
+            # Transform string coordinates into a tuple :
+            coord = self._signal._coord_to_tuple(coord)
+            # Get index of the signal :
+            idx_sig = self._signal._get_signal_index(signal)
+            # Update plot only if needed :
+            if idx_sig != self._signal._index:
+                self._sig_index.setValue(idx_sig)
+                # self._fcn_set_signal()
+            # Finally, select annotations :
+            self._signal.select_annotation(signal, coord)
+
+    def _fcn_text_edited(self):
+        """Update text label of selected row."""
+        row = self._annot_table.currentRow()
+        if row >= 0:
+            # Get coordinates, signal and annotation text :
+            coord = self._annot_table.item(row, 0).text()
+            signal = self._annot_table.item(row, 1).text()
+            text = self._annot_table.item(row, 2).text()
+            self._signal.set_text(signal, coord, text)
