91cd13b30894c8f23a04122eb82683e1e6c53d1f
EtienneCmb <e.combrisson@gmail.com>
2017-09-16 22:21:41 +0200

    Fix example

--- examples/signal/00_basic.py
+++ examples/signal/00_basic.py
@@ -1,10 +1,11 @@
 import numpy as np
-from visbrain import GridSignals
+from visbrain import Signal
 
 kwargs = {'xlabel': 'xlabel', 'ylabel': 'ylabel', 'title': 'title',
           'display_grid': False, 'color': 'darkgray', 'symbol': 'x',
           'title_font_size': 20, 'axis_font_size': 18, 'tick_font_size': 8,
           'axis_color': 'blue'}
 
-data = np.exp(np.random.rand(10000, 10, 5) + 10.)
-GridSignals(data, axis=0, **kwargs).show()
+data = 100 * np.random.rand(4000, 2, 40) + 10.
+# data = np.sin(2 * np.pi * 4 * np.arange(10000) / 1024) + 100
+Signal(data, axis=0, sf = 1024., **kwargs).show()
