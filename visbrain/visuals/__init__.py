75ecdb4d2dd91a994e1f75bd8c03563f04345b86
EtienneCmb <e.combrisson@gmail.com>
2017-09-16 22:21:15 +0200

    GridSignalsMesh -> GridSignalMesh

--- visbrain/visuals/__init__.py
+++ visbrain/visuals/__init__.py
@@ -3,4 +3,5 @@ from .BrainVisual import BrainMesh
 from .ConnectVisual import ConnectMesh
 from .PicVisual import PicMesh
 from .TopoVisual import TopoMesh
+from .GridSignalVisual import GridSignalMesh
 from .cbar import *
