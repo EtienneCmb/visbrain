5cfd57c0cb2171374874ec50052ee6208ca72934
EtienneCmb <e.combrisson@gmail.com>
2017-09-17 10:50:47 +0200

    Remove unused `Time window` gui spin + pep8 and flake8

--- visbrain/sleep/interface/uiElements/uiInfo.py
+++ visbrain/sleep/interface/uiElements/uiInfo.py
@@ -13,30 +13,30 @@ class uiInfo(object):
 
     def __init__(self):
         """Init."""
-        # Time window changed :
-        self._infoTime.valueChanged.connect(self._fcn_infoUpdate)
-        self._infoTime.setKeyboardTracking(False)
+        pass
 
     def _fcn_infoUpdate(self):
         """Complete the table sleep info."""
+        table = self._infoTable
         # Get sleep stats :
         stats = sleepstats(self._hyp.gui_to_hyp(), self._sf)
 
         # Add global informations to stats dict
-        stats['Filename'] = path.basename(self._file) if self._file is not None else ''
+        is_file = isinstance(self._file, str)
+        stats['Filename'] = path.basename(self._file) if is_file else ''
         stats['Sampling frequency'] = str(self._sfori) + " Hz"
         stats['Down-sampling'] = str(self._sf) + " Hz"
 
         self._keysInfo = [''] * len(stats)
         self._valInfo = [''] * len(stats)
         # Check line number:
-        self._infoTable.setRowCount(len(stats))
+        table.setRowCount(len(stats))
         # Fill table :
         for num, (k, v) in enumerate(stats.items()):
             # Add keys :
-            self._infoTable.setItem(int(num), 0, QtWidgets.QTableWidgetItem(k))
+            table.setItem(int(num), 0, QtWidgets.QTableWidgetItem(k))
             # Add values :
-            self._infoTable.setItem(int(num), 1, QtWidgets.QTableWidgetItem(str(v)))
+            table.setItem(int(num), 1, QtWidgets.QTableWidgetItem(str(v)))
             # Remember variables :
             self._keysInfo[int(num)] = k
             self._valInfo[int(num)] = str(v)
