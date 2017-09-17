a23cb3cae8f1b4f54dab770a0d50b6ca2429b4dc
EtienneCmb <e.combrisson@gmail.com>
2017-09-17 23:25:11 +0200

    Example for grid debugging

--- examples/signal/00_basic.py
+++ examples/signal/00_basic.py
@@ -7,6 +7,6 @@ kwargs = {'xlabel': 'xlabel', 'ylabel': 'ylabel', 'title': 'title',
           'axis_color': 'blue', 'bgcolor': 'white', 'enable_grid': True,
           'display_signal': False}
 
-data = 100 * np.random.rand(4000, 2, 40) + 10.
+data = 100 * np.random.rand(7, 4000) + 10.
 # data = np.sin(2 * np.pi * 4 * np.arange(10000) / 1024) + 100
-Signal(data, axis=0, sf = 1024., **kwargs).show()
+Signal(data, axis=-1, sf = 1024., **kwargs).show()
