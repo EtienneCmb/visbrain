cc46b20612161b21513038bb9d7a55217d39f249
EtienneCmb <e.combrisson@gmail.com>
2017-09-19 16:13:22 +0200

    Add small function to time a code

--- visbrain/utils/memory.py
+++ visbrain/utils/memory.py
@@ -5,7 +5,7 @@ Taken from the numpy tricks : http://ipython-books.github.io/featured-01/
 import numpy as np
 
 
-__all__ = ['id', 'arrays_share_data']
+__all__ = ('id', 'arrays_share_data', 'code_timer')
 
 
 def id(x):
@@ -24,3 +24,24 @@ def get_data_base(arr):
 def arrays_share_data(x, y):
     """Return if two arrays share an offset."""
     return get_data_base(x) is get_data_base(y)
+
+
+def code_timer(previous=0., verbose=True, prefix=''):
+    """Time code execution.
+
+    Parameters
+    ----------
+    previous : float | 0.
+        Previous code timing.
+    verbose : bool | True
+        Print time difference.
+    prefix : string | ''
+        Prefix to add before printing.
+    """
+    from time import time
+
+    current = time()
+    st = abs(current - previous)
+    if verbose:
+        print(prefix, st)
+    return current
