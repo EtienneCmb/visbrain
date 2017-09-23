02a6a5e1ad1a697fb176d41ef19737a05015fc6f
EtienneCmb <e.combrisson@gmail.com>
2017-09-23 10:23:29 +0200

    Bump to version 0.3.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pip
 from pip.req import parse_requirements
 from optparse import Option
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 NAME = 'visbrain'
 AUTHOR = "Visbrain developpers"
 MAINTAINER = "Etienne Combrisson"
