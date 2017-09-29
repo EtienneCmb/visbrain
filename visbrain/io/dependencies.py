cd16277ad41f398537ccf38fc80465260d081727
EtienneCmb <e.combrisson@gmail.com>
2017-09-29 02:29:28 +0200

    Test if opengl is installed

--- visbrain/io/dependencies.py
+++ visbrain/io/dependencies.py
@@ -1,6 +1,6 @@
 """Test if dependencies are installed."""
 
-__all__ = ('is_mne_installed', 'is_nibabel_installed')
+__all__ = ('is_mne_installed', 'is_nibabel_installed', 'is_opengl_installed')
 
 
 def is_mne_installed():
@@ -16,6 +16,15 @@ def is_nibabel_installed():
     """Test if nibabel is installed."""
     try:
         import nibabel
+        return True
+    except:
+        return False
+
+
+def is_opengl_installed():
+    """Test if open gl is installed."""
+    try:
+        import OpenGL.GL as GL
         return True
     except:
         return False
