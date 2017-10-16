c82ba948aee6bb8d119e313dbf53700e12435ad0
Etienne Combrisson <e.combrisson@gmail.com>
2017-10-16 20:49:18 +0200

    Improve TimeSeriesObj

--- visbrain/objects/ts_obj.py
+++ visbrain/objects/ts_obj.py
@@ -1,3 +1,4 @@
+"""Base class for the time-series object."""
 import numpy as np
 
 from vispy import scene
@@ -13,11 +14,38 @@ class TimeSeriesObj(VisbrainObject):
 
     Parameters
     ----------
+    name : string
+        Name of the time-series object.
+    data : array_like
+        Array of time-series of shape (n_sources, n_time_points)
+    xyz : array_like
+        The 3-D center location  of each time-series of shape (n_sources, 3).
+    select : array_like | None
+        Select the time-series to display. Should be a vector of bolean values
+        of shape (n_sources,).
+    line_width : float | 1.5
+        Time-series' line width.
+    color : array_like/tuple/string | 'white'
+        Time-series' color.
+    amplitude : float | 6.
+        Graphical amplitude of the time-series.
+    width : float | 20.
+        Graphical width of the time-series.
+    alpha : float | 1.
+        Time-series transparency.
+    translate : tuple | (0., 0., 1.)
+        Translate the time-series over the (x, y, z) axes.
+    transform : VisPy.visuals.transforms | None
+        VisPy transformation to set to the parent node.
+    parent : VisPy.parent | None
+        Line object parent.
+    _z : float | 10.
+        In case of (n_sources, 2) use _z to specify the elevation.
     """
 
     def __init__(self, name, data, xyz, select=None, line_width=1.5,
-                 color='white', amplitude=6., width=20., translate=(0., 0., 1.),
-                 transform=None, parent=None, _z=-10.):
+                 color='white', amplitude=6., width=20., alpha=1.,
+                 translate=(0., 0., 1.), transform=None, parent=None, _z=-10.):
         """Init."""
         # Init Visbrain object base class :
         VisbrainObject.__init__(self, name, parent, transform)
@@ -38,12 +66,19 @@ class TimeSeriesObj(VisbrainObject):
         # Amplitude / width :
         assert isinstance(amplitude, float) and isinstance(width, float)
         self._amplitude, self._width = amplitude, width
-        # DXYZ :
+        # Translate :
+        assert len(translate) == 3
         tr = vist.STTransform(translate=translate)
+        self._translate = translate
+        # Line width :
+        self._line_width = line_width
+        # Color :
+        self._color = color2vb(color, alpha=alpha)
+        self._alpha = alpha
 
         # _______________________ LINE _______________________
         self._ts = visuals.Line(name='TimeSeriesObjLine', parent=self._node,
-                                width=line_width, color=color2vb(color))
+                                width=line_width, color=self._color)
         self._ts.transform = tr
         self._build_line()
 
@@ -79,6 +114,38 @@ class TimeSeriesObj(VisbrainObject):
         dist = np.sqrt(np.sum(d_mean ** 2))
         return scene.cameras.TurntableCamera(center=d_mean, scale_factor=dist)
 
+    ###########################################################################
+    ###########################################################################
+    #                             PROPERTIES
+    ###########################################################################
+    ###########################################################################
+
+    # ----------- WIDTH -----------
+    @property
+    def width(self):
+        """Get the width value."""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """Set width value."""
+        assert isinstance(value, (int, float))
+        self._width = value
+        self._build_line()
+
+    # ----------- AMPLITUDE -----------
+    @property
+    def amplitude(self):
+        """Get the amplitude value."""
+        return self._amplitude
+
+    @amplitude.setter
+    def amplitude(self, value):
+        """Set amplitude value."""
+        assert isinstance(value, (int, float))
+        self._amplitude = value
+        self._build_line()
+
     # ----------- COLOR -----------
     @property
     def color(self):
@@ -93,6 +160,20 @@ class TimeSeriesObj(VisbrainObject):
         self._color = color
         self.update()
 
+    # ----------- ALPHA -----------
+    @property
+    def alpha(self):
+        """Get the alpha value."""
+        return self._alpha
+
+    @alpha.setter
+    def alpha(self, value):
+        """Set alpha value."""
+        assert isinstance(value, (int, float)) and (0. <= value <= 1.)
+        self._ts.color[..., -1] = value
+        self._alpha = value
+        self.update()
+
     # ----------- TRANSLATE -----------
     @property
     def translate(self):
@@ -102,10 +183,10 @@ class TimeSeriesObj(VisbrainObject):
     @translate.setter
     def translate(self, value):
         """Set translate value."""
-        if len(value) == 3:
-            self._ts.transform.translate = value
-            self._translate = value
-            self.update()
+        assert len(value) == 3
+        self._ts.transform.translate = value
+        self._translate = value
+        self.update()
 
     # ----------- LINE_WIDTH -----------
     @property
@@ -116,13 +197,25 @@ class TimeSeriesObj(VisbrainObject):
     @line_width.setter
     def line_width(self, value):
         """Set line_width value."""
-        if isinstance(value, (int, float)):
-            self._ts.width = value
-            self._line_width = value
+        assert isinstance(value, (int, float))
+        self._ts._width = value
+        self._line_width = value
+        self._ts.update()
 
 
 class CombineTimeSeries(CombineObjects):
-    """docstring for CombineTimeSeries"""
+    """Combine time-series objects.
 
-    def __init__(self):
-        pass
+    Parameters
+    ----------
+    tsobjs : TimeSeriesObj/list | None
+        List of time-series objects.
+    select : string | None
+        The name of the time-series object to select.
+    parent : VisPy.parent | None
+        Line object parent.
+    """
+
+    def __init__(self, tsobjs=None, select=None, parent=None):
+        """Init."""
+        CombineObjects.__init__(self, TimeSeriesObj, tsobjs, select, parent)
