51604714cafffc904752f9e4ed3ffbd4128cc681
Etienne Combrisson <e.combrisson@gmail.com>
2017-10-18 19:59:50 +0200

    Projection configuration

--- visbrain/brain/base/projection.py
+++ visbrain/brain/base/projection.py
@@ -1,6 +1,10 @@
+import logging
 import numpy as np
 
-from ...utils import array2colormap
+from ...utils import array2colormap, color2vb
+
+
+logger = logging.getLogger('visbrain')
 
 
 __all__ = ['Projections']
@@ -14,26 +18,26 @@ class Projections(object):
     """
 
     def __init__(self, t_radius=10.0, t_projecton='brain', t_contribute=False,
-                 t_projectas='activity', t_fitto='brain', **kwargs):
+                 t_projectas='activity', t_mask_color='orange', **kwargs):
         """Init."""
         self._tobj = {}
         self._tradius = t_radius
         self._tprojecton = t_projecton
         self._tprojectas = t_projectas
         self._tcontribute = t_contribute
-        self._tfitto = 'brain'
         self._idxmasked = None
+        self._proj_mask_color = color2vb(t_mask_color)
         self._modproj = None
 
     # ======================================================================
     # PROJECTIONS
     # ======================================================================
-    def _findVertices(self, obj):
+    def _get_obj_vertices(self, obj):
         """Find the vertices from the object."""
         if obj in self._tobj.keys():
             return self._tobj[obj].mesh.get_vertices
         else:
-            raise ValueError(obj + " not found. USe : " +
+            raise ValueError(obj + " not found. Use : " +
                              list(self._tobj.keys()))
 
     def _sourcesProjection(self):
@@ -57,26 +61,30 @@ class Projections(object):
                              "contributing sources per vertex.")
 
         # =============== VERTICES ===============
-        v = self._findVertices(self._tprojecton)
+        v = self._get_obj_vertices(self._tprojecton)
         self._vsh = v.shape
 
         # ============= MODULATIONS =============
         r, c = self._tradius, self._tcontribute
+        log_str = ("Project {} onto the %s using a %s "
+                   "radius") % (self._tprojecton, str(self._tradius))
         if self._tprojectas == 'activity':
-            mod = self.sources._modulation(v, r, c)
+            logger.info(log_str.format("source's activity"))
+            mod = self.sources.project_modulation(v, r, c)
         elif self._tprojectas == 'repartition':
-            mod = self.sources._repartition(v, r, c)
+            logger.info(log_str.format("source's repartition"))
+            mod = self.sources.project_repartition(v, r, c)
         self._modproj = mod
         self.sources._minmax = (mod.min(), mod.max())
 
         # ============= MASKED =============
-        if self.sources and (self._idxmasked is None):
-            self._idxmasked = self.sources._MaskedEucl(v, self._tradius, c)
+        if self.sources.is_masked and (self._idxmasked is None):
+            self._idxmasked = self.sources.get_masked_index(v, self._tradius, c)
 
         # ============= COLOR =============
-        self._proj2Color(init=True)
+        self._projection_to_color()
 
-    def _proj2Color(self, init=False):
+    def _projection_to_color(self):
         """Turn the projection into colormap."""
         # Get color arguents :
         kwargs = self.cbobjs._objs['Projection'].to_kwargs()
@@ -84,28 +92,14 @@ class Projections(object):
         color = array2colormap(self._modproj, **kwargs)
         color[self._modproj.mask, ...] = 1.
 
-        if self.sources:
+        if self.sources.is_masked:
             # Find only non-colored vertex :
             idxcol = np.logical_and(self._idxmasked, self._modproj.mask)
             # Set them color to the mask color :
-            color[idxcol, ...] = self.sources.smaskcolor
+            color[idxcol, ...] = self._proj_mask_color
 
         # ============= MESH =============
         self._tobj[self._tprojecton].mesh.set_color(data=color)
-
-        if init:
-            # Disconnect interactions :
-            self.cbqt._disconnect()
-            # Enable projection in the cbar object selection :
-            self.cbqt.setEnabled('Projection', True)
-            self.cbqt.select('Projection')
-            # Update variables :
-            self.cbqt._fcn_ChangeObj()
-            # Enable to display cbar from the menu :
-            self._fcn_menu_disp_cbar()
-            # Link the colorbase with projections :
-            self.cbqt.link('Projection', self._fcn_link_proj,
-                           self._fcn_minmax_proj)
 
     def _cleanProj(self):
         """Clean projection variables."""
