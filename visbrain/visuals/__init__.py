3b83412533674293b4d2b7da02ea3d2609b1f214
Etienne Combrisson <e.combrisson@gmail.com>
2017-10-24 13:43:58 +0200

    BIG MOVE : remove ConnectVisual

--- visbrain/visuals/__init__.py
+++ visbrain/visuals/__init__.py
@@ -1,7 +1,6 @@
 """Visual objects."""
 from .BrainVisual import BrainMesh
 from .cbar import *
-from .ConnectVisual import ConnectMesh
 from .GridSignalVisual import GridSignal
 from .PicVisual import PicMesh
 from .TFmapsVisual import TFmapsMesh
