88b297dd9280bbbbc30e7f28c4a7b916a9089e6d
Etienne Combrisson <e.combrisson@gmail.com>
2017-10-31 09:58:53 +0100

    Gray as the default mask_color

--- visbrain/brain/base/projection.py
+++ visbrain/brain/base/projection.py
@@ -20,7 +20,7 @@ class Projections(object):
 
     def __init__(self, project_radius=10.0, project_on='brain',
                  project_contribute=False, project_type='activity',
-                 project_mask_color='orange', **kwargs):
+                 project_mask_color='gray', **kwargs):
         """Init."""
         logger.debug("Problème avec proj_obj : on enregistre les mesh dans "
                      "proj_obj pour récupérer les vertices + définir ensuite "
