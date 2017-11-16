24f713561567cdb64e5201431dc977b428ac2c7c
Etienne Combrisson <e.combrisson@gmail.com>
2017-11-16 10:59:45 -0500

    Add profiler level

--- visbrain/utils/logging.py
+++ visbrain/utils/logging.py
@@ -5,6 +5,7 @@ https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
 """
 import logging
 import sys
+import re
 
 __all__ = ['set_log_level']
 
@@ -13,18 +14,18 @@ BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
 RESET_SEQ = "\033[0m"
 COLOR_SEQ = "\033[1;%dm"
 BOLD_SEQ = "\033[1m"
-LOGGING_TYPES = dict(DEBUG=logging.DEBUG, INFO=logging.INFO,
-                     WARNING=logging.WARNING, ERROR=logging.ERROR,
-                     CRITICAL=logging.CRITICAL)
 COLORS = {
     'DEBUG': GREEN,
+    'PROFILER': MAGENTA,
     'INFO': WHITE,
     'WARNING': YELLOW,
     'ERROR': RED,
     'CRITICAL': RED,
 }
 FORMAT = {'compact': "$BOLD%(levelname)s | %(message)s",
-          'spacy': "$BOLD%(levelname)-19s$RESET | %(message)s"
+          'spacy': "$BOLD%(levelname)-19s$RESET | %(message)s",
+          'visbrain': "$BOLD%(name)s-%(levelname)-19s$RESET | %(message)s",
+          'print': "%(message)s",
           }
 
 
@@ -38,11 +39,27 @@ class _Formatter(logging.Formatter):
 
     def __init__(self, format_type='compact'):
         logging.Formatter.__init__(self, FORMAT[format_type])
+        self._format_type = format_type
 
     def format(self, record):
         name = record.levelname
+        msg = record.getMessage()
+        # If * in msg, set it in RED :
+        if '*' in msg:
+            regexp = '\*.*?\*'
+            re_search = re.search(regexp, msg).group()
+            to_color = COLOR_SEQ % (30 + RED) + re_search + COLOR_SEQ % (
+                30 + WHITE) + RESET_SEQ
+            msg_color = re.sub(regexp, to_color, msg)
+            msg_color += RESET_SEQ
+            record.msg = msg_color
+        # Set level color :
         levelname_color = COLOR_SEQ % (30 + COLORS[name]) + name + RESET_SEQ
         record.levelname = levelname_color
+        # if record.levelno == 11:
+        #     logging.Formatter.__init__(self, FORMAT['print'])
+        # else:
+        #     logging.Formatter.__init__(self, FORMAT[self._format_type])
         return formatter_message(logging.Formatter.format(self, record))
 
 
@@ -52,15 +69,50 @@ class _StreamHandler(logging.StreamHandler):
     def __init__(self):
         logging.StreamHandler.__init__(self, sys.stderr)
         self.setFormatter(_lf)
+        self._str_pattern = None
+        self.emit = self._visbrain_emit
+
+    def _visbrain_emit(self, record):
+        msg = record.getMessage()
+        test = self._match_pattern(record, msg)
+        if test:
+            record.msg = test
+            return logging.StreamHandler.emit(self, record)
+        else:
+            return
+
+    def _match_pattern(self, record, message):
+        if isinstance(self._str_pattern, str):
+            if re.search(self._str_pattern, message):
+                sub = '*{}*'.format(self._str_pattern)
+                return re.sub(self._str_pattern, sub, message)
+            else:
+                return ''
+        else:
+            return message
 
 
 logger = logging.getLogger('visbrain')
 _lf = _Formatter()
 _lh = _StreamHandler()  # needs _lf to exist
 logger.addHandler(_lh)
+PROFILER_LEVEL_NUM = 1
+logging.addLevelName(PROFILER_LEVEL_NUM, "PROFILER")
 
 
-def set_log_level(verbose=None):
+def profiler_fcn(self, message, *args, **kws):
+    # Yes, logger takes its '*args' as 'args'.
+    if self.isEnabledFor(PROFILER_LEVEL_NUM):
+        self._log(PROFILER_LEVEL_NUM, message, args, **kws)
+
+
+logging.Logger.profiler = profiler_fcn
+LOGGING_TYPES = dict(DEBUG=logging.DEBUG, INFO=logging.INFO,
+                     WARNING=logging.WARNING, ERROR=logging.ERROR,
+                     CRITICAL=logging.CRITICAL, PROFILER=PROFILER_LEVEL_NUM)
+
+
+def set_log_level(verbose=None, match=None):
     """Convenience function for setting the logging level.
 
     This function comes from the PySurfer package. See :
@@ -69,21 +121,21 @@ def set_log_level(verbose=None):
     Parameters
     ----------
     verbose : bool, str, int, or None
-        The verbosity of messages to print. If a str, it can be either DEBUG,
-        INFO, WARNING, ERROR, or CRITICAL. Note that these are for
-        convenience and are equivalent to passing in logging.DEBUG, etc.
-        For bool, True is the same as 'INFO', False is the same as 'WARNING'.
-        If None, the environment variable MNE_LOG_LEVEL is read, and if
-        it doesn't exist, defaults to INFO.
-    return_old_level : bool
-        If True, return the old verbosity level.
+        The verbosity of messages to print. If a str, it can be either
+        PROFILER, DEBUG, INFO, WARNING, ERROR, or CRITICAL.
     """
     # if verbose is None:
     #     verbose = "INFO"
+    logger = logging.getLogger('visbrain')
     if isinstance(verbose, bool):
         verbose = 'INFO' if verbose else 'WARNING'
-    if isinstance(verbose, str) and (verbose.upper() in LOGGING_TYPES):
-        verbose = verbose.upper()
-        verbose = LOGGING_TYPES[verbose]
-        logger = logging.getLogger('visbrain')
-        logger.setLevel(verbose)
+    if isinstance(verbose, str):
+        if (verbose.upper() in LOGGING_TYPES):
+            verbose = verbose.upper()
+            verbose = LOGGING_TYPES[verbose]
+            logger.setLevel(verbose)
+        else:
+            raise ValueError("verbose must be in "
+                             "%s" % ', '.join(LOGGING_TYPES))
+    if isinstance(match, str):
+        _lh._str_pattern = match
