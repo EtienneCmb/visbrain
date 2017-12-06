71c0f8c68f55ab76580dbb0aa77f380fb53f3624
Etienne Combrisson <e.combrisson@gmail.com>
2017-12-05 22:37:31 -0500

    Add title + how to find index

--- examples/objects/ex_roi_object.py
+++ examples/objects/ex_roi_object.py
@@ -24,6 +24,9 @@ mat = np.load(download_file('xyz_sample.npz'))
 xyz, subjects = mat['xyz'], mat['subjects']
 data = np.random.uniform(low=-1., high=1., size=(xyz.shape[0],))
 
+# =============================================================================
+#                              MAIN SCENE
+# =============================================================================
 print("-> Create a scene. By default, we fix the top view of the camera")
 CAM_STATE = dict(azimuth=0,        # azimuth angle
                  elevation=90,     # elevation angle
@@ -34,14 +37,41 @@ CBAR_STATE = dict(cbtxtsz=12, txtsz=10., width=.1, cbtxtsh=3.,
                   rect=(-.3, -2., 1., 4.))
 sc = SceneObj(camera_state=CAM_STATE, bgcolor=(.1, .1, .1), size=(1400, 1000))
 
+# =============================================================================
+#                          FIND INDEX OF AN ROI
+# =============================================================================
+"""Here, we illustrate how to find the integer index of the ROI to plot
+"""
+# Method 1 : save all ROI in an excel file and search manually the ROI
+roi_to_find1 = RoiObj('brodmann')             # Use Brodmann areas
+ref_brod = roi_to_find1.get_labels(vb_path)   # Save Brodmann
+roi_to_find1('aal')                           # Switch to AAL
+ref_aal = roi_to_find1.get_labels(vb_path)    # Save AAL
+roi_to_find1('talairach')                     # Switch to Talairach
+ref_tal = roi_to_find1.get_labels(vb_path)    # Save Talairach
+
+# Method 2 : use the `where_is` method
+roi_to_find1('brodmann')                      # Switch to Brodmann
+idx_ba6 = roi_to_find1.where_is('BA6')        # Find only BA6
+print(ref_brod.loc[idx_ba6])
+roi_to_find1('aal')                           # Switch to AAL
+idx_sma = roi_to_find1.where_is(['Supp Motor Area', '(L)'], union=False)
+
+# =============================================================================
+#                              BRAIN + BA6
+# =============================================================================
 print('\n-> Plot brodmann area 6')
 b_obj = BrainObj('B1')
 roi_brod = RoiObj('brodmann')
-roi_brod.select_roi(select=6)
+idx_ba6 = roi_brod.where_is('BA6')
+roi_brod.select_roi(select=idx_ba6)
 roi_brod.get_labels(save_to_path=vb_path)  # print available brodmann labels
 sc.add_to_subplot(roi_brod, row=0, col=0, title='Brodmann area 6')
 sc.add_to_subplot(b_obj, row=0, col=0, use_this_cam=True)
 
+# =============================================================================
+#                       MULTIPLE ROI + UNIQUE COLORS
+# =============================================================================
 print('\n-> Select and plot multiple ROI with random unique colors')
 roi_aal = RoiObj('aal')
 roi_aal.select_roi(select=[29, 30, 77, 78], unique_color=True, smooth=11)
@@ -49,6 +79,9 @@ roi_aal.get_labels(save_to_path=vb_path)  # save available AAL labels
 sc.add_to_subplot(roi_aal, row=0, col=1,
                   title='Select and plot multiple ROI with unique colors')
 
+# =============================================================================
+#                              SMA + FIXED COLORS
+# =============================================================================
 print('\n-> Plot SMA with fixed colors')
 roi_aal_2 = RoiObj('aal')
 colors = {19: 'slateblue', 20: 'olive'}
@@ -56,6 +89,9 @@ roi_aal_2.select_roi(select=[19, 20], smooth=11, roi_to_color=colors)
 sc.add_to_subplot(roi_aal_2, row=0, col=2,
                   title='Plot SMA with fixed colors')
 
+# =============================================================================
+#                        ANATOMICAL LOCATION OF SOURCES
+# =============================================================================
 print('\n-> Anatomical location of sources using an ROI object')
 # Define the ROI object :
 roi_tal = RoiObj('talairach')
@@ -70,6 +106,9 @@ sc.add_to_subplot(s_obj, row=1, col=0,
                   title='Anatomical location of sources')
 sc.add_to_subplot(roi_tal, row=1, col=0, use_this_cam=True)
 
+# =============================================================================
+#                         SELECT SOURCES INSIDE ROI'S
+# =============================================================================
 print('\n-> Select only sources inside BA 4, 6 and 8')
 # Define the ROI object :
 roi_brod_2 = RoiObj('brodmann')
@@ -84,6 +123,9 @@ sc.add_to_subplot(s_obj_2, row=1, col=1,
                   title='Plot only sources in BA4, 6 and 8')
 sc.add_to_subplot(roi_brod_2, row=1, col=1, use_this_cam=True)
 
+# =============================================================================
+#                   CORTICAL PROJECTION OF SOURCE'S ACTIVITY
+# =============================================================================
 print("\n-> Project source's activity onto ROI")
 # Define the ROI object :
 roi_brod_3 = RoiObj('aal')
