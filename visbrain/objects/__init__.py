dd24cf429d9edbcf619441f84fe5be61a80d8b46
Etienne Combrisson <e.combrisson@gmail.com>
2017-12-19 10:50:45 +0100

    Fix __init__ with picture3d_obj and ts3d_obj

--- visbrain/objects/__init__.py
+++ visbrain/objects/__init__.py
@@ -3,13 +3,13 @@ from .brain_obj import BrainObj  # noqa
 from .cbar_obj import ColorbarObj  # noqa
 from .connect_obj import ConnectObj, CombineConnect  # noqa
 from .image_obj import ImageObj  # noqa
-from .picture_obj import Picture3DObj, CombinePictures  # noqa
+from .picture3d_obj import Picture3DObj, CombinePictures  # noqa
 from .roi_obj import RoiObj, CombineRoi  # noqa
 from .scene_obj import SceneObj, VisbrainCanvas  # noqa
 from .source_obj import SourceObj, CombineSources  # noqa
 from .spec_obj import SpectrogramObj  # noqa
 from .tf_obj import TimeFrequencyMapObj  # noqa
-from .ts_obj import TimeSeries3DObj, CombineTimeSeries  # noqa
+from .ts3d_obj import TimeSeries3DObj, CombineTimeSeries  # noqa
 from .vector_obj import VectorObj, CombineVectors  # noqa
 from .visbrain_obj import VisbrainObject, CombineObjects  # noqa
 from .volume_obj import VolumeObj  # noqa
