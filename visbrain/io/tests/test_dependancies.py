836e90b3e0a238dd4ed01f097f98366598ddbe5c
Etienne Combrisson <e.combrisson@gmail.com>
2017-12-19 18:24:22 +0100

    Split I/O tests

---
+++ visbrain/io/tests/test_dependancies.py
@@ -0,0 +1,14 @@
+"""Test functions in dependancies.py."""
+from visbrain.io.dependencies import (is_mne_installed, is_nibabel_installed,
+                                      is_opengl_installed, is_pandas_installed,
+                                      is_faulthandler_installed)
+
+
+class TestDependencies(object):
+    """Test installed dependancies."""
+
+    def test_dependencies(self):
+        """Test function dependencies."""
+        for k in (is_mne_installed, is_nibabel_installed, is_opengl_installed,
+                  is_pandas_installed, is_faulthandler_installed):
+            assert isinstance(k(), bool)
