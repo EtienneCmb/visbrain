78e451fcd7a76fb861f3297b6de91a89cb517b7d
Etienne Combrisson <e.combrisson@gmail.com>
2017-12-29 18:49:16 +0100

    Bump to version 0.3.8

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import pip
 from pip.req import parse_requirements
 from optparse import Option
 
-__version__ = "0.3.7"
+__version__ = "0.3.8"
 NAME = 'visbrain'
 AUTHOR = "Visbrain developpers"
 MAINTAINER = "Etienne Combrisson"
