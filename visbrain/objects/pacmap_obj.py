edc4622552c6767edbf635e709b50ba229964eaf
Etienne Combrisson <e.combrisson@gmail.com>
2017-12-31 19:13:32 +0100

    PacmapObj basic configuration

--- visbrain/objects/pacmap_obj.py
+++ visbrain/objects/pacmap_obj.py
@@ -2,27 +2,37 @@
 import numpy as np
 
 from .image_obj import ImageObj
+from ..io.dependencies import is_tensorpac_installed
 
 
 class PacmapObj(ImageObj):
-    """Create a Phase-amplitude coupling object.
+    """Create a Phase-Amplitude Coupling (PAC) object.
+
+    The PAC is computed using the tensorpac package.
+
+    The Pacmap can be used to visualize :
+
+        * PAC, across time for a fixed phase and several amplitude frequencies
+        * PAC, across time for a fixed amplitude and several phase frequencies
+        * PAC is computed across time for several amplitudes and phase.
 
     Parameters
     ----------
     name : string | None
-        Name of the time-frequency object.
+        Name of the pacmap object.
     data : array_like
         Array of data of shape (N,)
     sf : float | 1.
         The sampling frequency.
-    nperseg : int | 256
-        Length of each segment. Argument pass to the `scipy.signal.spectrogram`
-        function.
-    overlap : float | 0.
-        Overlap between segemnts. Must be between 0. and 1.
-    window : string | 'hamming'
-        Desired window to use. Argument pass to the `scipy.signal.spectrogram`
-        function.
+    f_pha : list | [(2, 4), (5, 7), (8, 13)]
+        The phase vector.
+    f_amp : list | [(40, 60), (60, 100)]
+        The amplitude vector.
+    idpac : tuple | (4, 0, 0)
+        The PAC method to use.
+    n_window : int | None
+        Number of time points to consider when computing pac for a fixed phase
+        or amplitude.
     clim : tuple | None
         Colorbar limits. If None, `clim=(data.min(), data.max())`
     cmap : string | None
@@ -63,40 +73,59 @@ class PacmapObj(ImageObj):
     >>> spec.preview(axis=True)
     """
 
-    def __init__(self, name, data=None, sf=1., nperseg=256, overlap=0.,
-                 window='hamming', cmap='viridis', clim=None, vmin=None,
-                 under='gray', vmax=None, over='red', interpolation='nearest',
-                 max_pts=-1, parent=None, transform=None, verbose=None,
-                 **kw):
+    def __init__(self, name, data=None, sf=1., f_pha=[(2, 4), (5, 7), (8, 13)],
+                 f_amp=[(40, 60), (60, 100)], idpac=(4, 0, 0), n_window=None,
+                 cmap='viridis', clim=None, vmin=None, under='gray', vmax=None,
+                 over='red', interpolation='nearest', max_pts=-1, parent=None,
+                 transform=None, verbose=None, **kw):
         """Init."""
         # Initialize the image object :
         ImageObj.__init__(self, name, interpolation=interpolation,
                           max_pts=max_pts, parent=parent, transform=transform,
                           verbose=verbose, **kw)
 
-        # Compute spectrogram and set data to the ImageObj :
+        # Compute pacmap and set data to the ImageObj :
         if isinstance(data, np.ndarray):
-            self.set_data(data, sf, nperseg, overlap, window, clim, cmap,
+            self.set_data(data, sf, f_pha, f_amp, idpac, n_window, clim, cmap,
                           vmin, under, vmax, over)
 
-    def set_data(self, data, sf=1., nperseg=None, overlap=None,
-                 window='hamming', clim=None, cmap=None, vmin=None, under=None,
-                 vmax=None, over=None, **kwargs):
-        """Compute spectrogram and set data to the ImageObj."""
+    def set_data(self, data, sf=1., f_pha=[(2, 4), (5, 7), (8, 13)],
+                 f_amp=[(40, 60), (60, 100)], idpac=(4, 0, 0), n_window=None,
+                 clim=None, cmap=None, vmin=None, under=None, vmax=None,
+                 over=None, **kwargs):
+        """Compute pacmap and set data to the ImageObj."""
+        # ======================= CHECKING =======================
+        is_tensorpac_installed(raise_error=True)
+        from tensorpac import Pac
         assert isinstance(data, np.ndarray) and data.ndim == 1
         assert isinstance(sf, (int, float))
-        assert isinstance(nperseg, int)
-        assert 0. <= overlap < 1.
-        noverlap = int(round(overlap * nperseg))
+        time = np.arange(len(data))
+        # Switch between several plotting capabilities :
+        _pha = len(f_pha) == 2 and all([isinstance(k, (int,
+                                                       float)) for k in f_pha])
+        _amp = len(f_amp) == 2 and all([isinstance(k, (int,
+                                                       float)) for k in f_amp])
+        if _pha or _amp:
+            assert isinstance(n_window, int)
+            sections = time.copy()[::n_window][1::]
+            time = np.array(np.array_split(time, sections)[0:-1]).mean(1).T
+            time /= sf
+            data = np.array(np.array_split(data, sections)[0:-1]).T
+        # Define the pac object and compute pac :
+        p = Pac(idpac=idpac, fpha=f_pha, famp=f_amp)
+        pac = np.squeeze(p.filterfit(sf, data, njobs=1, axis=0))
+        pac[np.isnan(pac)] = 0.
+        assert pac.ndim == 2
+        # Get x-axis and y-axis :
+        if _pha:
+            xaxis, yaxis = time, p.yvec
+        elif _amp:
+            xaxis, yaxis = time, p.xvec
+        else:
+            xaxis, yaxis = p.xvec, p.yvec
+        # Update color arguments :
         self._update_cbar_args(cmap, clim, vmin, vmax, under, over)
 
-        # Compute spectrogram :
-        kwargs['nperseg'] = nperseg
-        kwargs['noverlap'] = noverlap
-        kwargs['window'] = window
-        freqs, time, tf = spectrogram(data, sf, nperseg=nperseg,
-                                      noverlap=noverlap, window=window)
-
         # Set data to the image object :
-        ImageObj.set_data(self, tf, xaxis=time, yaxis=freqs,
+        ImageObj.set_data(self, pac, xaxis=xaxis, yaxis=yaxis,
                           **self.to_kwargs())
