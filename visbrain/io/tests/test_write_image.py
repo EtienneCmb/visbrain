487dc17202fd7ca139a57418a874b0b036f05f8f
Etienne Combrisson <e.combrisson@gmail.com>
2018-01-04 14:43:18 +0100

    Image testing

--- visbrain/io/tests/test_write_image.py
+++ visbrain/io/tests/test_write_image.py
@@ -2,14 +2,17 @@
 import numpy as np
 import pytest
 
-from visbrain.io import (write_fig_hyp, write_fig_spindles, write_fig_canvas,
-                         write_fig_pyqt)
+from visbrain.utils import generate_eeg
+from visbrain.io import (write_fig_hyp, write_fig_spindles,  # noqa
+                         write_fig_canvas, write_fig_pyqt)
 from visbrain.tests._tests_visbrain import _TestVisbrain
 
 
 class TestWriteImage(_TestVisbrain):
     """Test functions in write_image.py."""
 
+    @pytest.mark.xfail(reason="Failed if display not correctly configured",
+                       run=True, strict=False)
     def test_write_fig_hyp(self):
         """Test function write_fig_hyp."""
         hypno = np.repeat(np.arange(6), 100) - 1.
@@ -19,9 +22,17 @@ class TestWriteImage(_TestVisbrain):
         write_fig_hyp(hypno.copy(), sf, file=file)
         write_fig_hyp(hypno.copy(), sf, file=file_color, ascolor=True)
 
+    @pytest.mark.xfail(reason="Failed if display not correctly configured",
+                       run=True, strict=False)
     def test_write_fig_spindles(self):
         """Test function write_fig_spindles."""
-        pass
+        sf, n_pts = 100., 10014
+        hypno = np.repeat(np.arange(6), 1669) - 1.
+        sig = np.squeeze(generate_eeg(sf=sf, n_pts=n_pts, random_state=1)[0])
+        file = self.to_tmp_dir('write_fig_spindles.png')
+        file_hyp = self.to_tmp_dir('write_fig_spindles_hypno.png')
+        write_fig_spindles(sig, sf, file=file, thr=.1)
+        write_fig_spindles(sig, sf, hypno=hypno, file=file_hyp, thr=.1)
 
     @pytest.mark.skip('Should be tested inside modules or objects.')
     def test_write_fig_canvas(self):
