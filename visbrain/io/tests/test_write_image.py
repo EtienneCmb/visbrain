77128839c450284e4983b4029f2e795fa24be8aa
Etienne Combrisson <e.combrisson@gmail.com>
2018-01-04 14:28:46 +0100

    Start writing tests for write_image

--- visbrain/io/tests/test_write_image.py
+++ visbrain/io/tests/test_write_image.py
@@ -4,14 +4,20 @@ import pytest
 
 from visbrain.io import (write_fig_hyp, write_fig_spindles, write_fig_canvas,
                          write_fig_pyqt)
+from visbrain.tests._tests_visbrain import _TestVisbrain
 
 
-class TestWriteImage(object):
+class TestWriteImage(_TestVisbrain):
     """Test functions in write_image.py."""
 
     def test_write_fig_hyp(self):
         """Test function write_fig_hyp."""
-        pass
+        hypno = np.repeat(np.arange(6), 100) - 1.
+        sf = 1.
+        file = self.to_tmp_dir('hypno_write_fig_hyp.png')
+        file_color = self.to_tmp_dir('hypno_write_fig_hyp_color.png')
+        write_fig_hyp(hypno.copy(), sf, file=file)
+        write_fig_hyp(hypno.copy(), sf, file=file_color, ascolor=True)
 
     def test_write_fig_spindles(self):
         """Test function write_fig_spindles."""
