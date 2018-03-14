b575f4db873ce4b5eabd13c206ccf6f9ad297815
Etienne Combrisson <e.combrisson@gmail.com>
2018-03-14 15:10:28 +0100

    Remove self._sf for writing hypno

--- visbrain/sleep/interface/ui_elements/ui_menu.py
+++ visbrain/sleep/interface/ui_elements/ui_menu.py
@@ -99,11 +99,9 @@ class UiMenu(HelpMenu):
 
             # Switch between differents types :
             if ext == '.hyp':
-                write_hypno_hyp(filename, self._hypno, self._sf, self._sfori,
-                                self._N)
+                write_hypno_hyp(filename, self._hypno, self._sfori, self._N)
             elif ext == '.txt':
-                write_hypno_txt(filename, self._hypno, self._sf, self._sfori,
-                                self._N, 1)
+                write_hypno_txt(filename, self._hypno, self._sfori, self._N, 1)
             else:
                 raise ValueError("Not a valid extension")
 
