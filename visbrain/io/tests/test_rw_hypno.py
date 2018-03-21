cbe8e0f7a7cf8acbd7e74bb37a4e9849420af8d1
Etienne Combrisson <e.combrisson@gmail.com>
2018-03-21 19:47:30 +0100

    Test for writing hypno

--- visbrain/io/tests/test_rw_hypno.py
+++ visbrain/io/tests/test_rw_hypno.py
@@ -3,9 +3,9 @@ import numpy as np
 
 from visbrain.tests._tests_visbrain import _TestVisbrain
 from visbrain.io.rw_hypno import (hypno_time_to_sample, hypno_sample_to_time,
-                                  oversample_hypno, write_hypno_txt,
-                                  write_hypno_hyp, write_hypno_xlsx,
-                                  read_hypno, read_hypno_hyp, read_hypno_txt)
+                                  oversample_hypno, write_hypno, read_hypno)
+
+versions = dict(time=['.txt', '.csv', '.xlsx'], sample=['.txt', '.hyp'])
 
 
 class TestRwHypno(_TestVisbrain):
@@ -38,39 +38,35 @@ class TestRwHypno(_TestVisbrain):
         to_hyp = np.array([-1, -1, 4, 4, 2, 2, 3, 3, 0, 0, 0, 0])
         assert np.array_equal(hyp_over, to_hyp)
 
-    def test_write_hypno_txt(self):
+    def test_write_hypno(self):
         """Test function write_hypno_txt."""
         hyp = self._get_hypno()
-        write_hypno_txt(self.to_tmp_dir('hyp.txt'), hyp, 1000., 5000)
+        sf, npts = 1., len(hyp)
+        time = np.arange(npts) / sf
+        info = {'Info_1': 10, 'Info_2': 'coco', 'Info_3': 'veut_un_gateau'}
+        for k in versions.keys():
+            for e in versions[k]:
+                filename = self.to_tmp_dir('hyp_test_' + k + e)
+                write_hypno(filename, hyp, version=k, sf=sf, npts=npts,
+                            window=1., time=time, info=info)
 
-    def test_write_hypno_hyp(self):
-        """Test function write_hypno_hyp."""
-        hyp = self._get_hypno()
-        write_hypno_hyp(self.to_tmp_dir('hyp.hyp'), hyp, 1000., 5000)
+    # def test_read_hypno(self):
+    #     """Test function read_hypno."""
+    #     # TXT version :
+    #     hyp_txt, sf_txt = read_hypno(self.to_tmp_dir('hyp.txt'))
+    #     # HYP version :
+    #     hyp_hyp, sf_hyp = read_hypno(self.to_tmp_dir('hyp.hyp'))
+    #     # XLSX version :
+    #     time = np.arange(len(self._get_hypno())) / 100.
+    #     hyp_xlsx, sf_xlsx = read_hypno(self.to_tmp_dir('hyp.xlsx'), time=time)
+    #     assert np.array_equal(hyp_txt, hyp_hyp)
+    #     assert np.array_equal(hyp_txt, hyp_xlsx)
+    #     assert sf_txt == sf_hyp
 
-    def test_write_hypno_xlsx(self):
-        """Test function write_hypno_xlsx."""
-        hyp = self._get_hypno()
-        time = np.arange(len(hyp)) / 100.
-        write_hypno_xlsx(self.to_tmp_dir('hyp.xlsx'), hyp, time)
+    # def test_read_hypno_hyp(self):
+    #     """Test function read_hypno_hyp."""
+    #     read_hypno_hyp(self.to_tmp_dir('hyp.hyp'))
 
-    def test_read_hypno(self):
-        """Test function read_hypno."""
-        # TXT version :
-        hyp_txt, sf_txt = read_hypno(self.to_tmp_dir('hyp.txt'))
-        # HYP version :
-        hyp_hyp, sf_hyp = read_hypno(self.to_tmp_dir('hyp.hyp'))
-        # XLSX version :
-        time = np.arange(len(self._get_hypno())) / 100.
-        hyp_xlsx, sf_xlsx = read_hypno(self.to_tmp_dir('hyp.xlsx'), time=time)
-        assert np.array_equal(hyp_txt, hyp_hyp)
-        assert np.array_equal(hyp_txt, hyp_xlsx)
-        assert sf_txt == sf_hyp
-
-    def test_read_hypno_hyp(self):
-        """Test function read_hypno_hyp."""
-        read_hypno_hyp(self.to_tmp_dir('hyp.hyp'))
-
-    def test_read_hypno_txt(self):
-        """Test function read_hypno_txt."""
-        read_hypno_txt(self.to_tmp_dir('hyp.txt'))
+    # def test_read_hypno_txt(self):
+    #     """Test function read_hypno_txt."""
+    #     read_hypno_txt(self.to_tmp_dir('hyp.txt'))
