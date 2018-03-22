013e11cd35fc05739e2ec4bc2de7e806cccfcc6e
Etienne Combrisson <e.combrisson@gmail.com>
2018-03-22 17:48:30 +0100

    Fix test rw_utils

--- visbrain/io/tests/test_rw_utils.py
+++ visbrain/io/tests/test_rw_utils.py
@@ -1,4 +1,5 @@
 """Test functions in rw_utils.py."""
+import numpy as np
 from visbrain.io.rw_utils import get_file_ext, safety_save
 from visbrain.tests._tests_visbrain import _TestVisbrain
 
@@ -8,12 +9,13 @@ class TestRwUtils(_TestVisbrain):
 
     def test_get_file_ext(self):
         """Test function get_file_ext."""
-        file, ext = get_file_ext(self.to_tmp_dir('hyp_data.txt'))
+        np.savetxt(self.to_tmp_dir('test_rw_utils.txt'), np.arange(100))
+        file, ext = get_file_ext(self.to_tmp_dir('test_rw_utils.txt'))
         assert file[-1] != '.'
         assert ext == '.txt'
 
     def test_safety_save(self):
         """Test function safety_save."""
-        file = safety_save(self.to_tmp_dir('hyp_data.txt'))
-        file_txt, _ = get_file_ext(self.to_tmp_dir('hyp_data.txt'))
+        file = safety_save(self.to_tmp_dir('test_rw_utils.txt'))
+        file_txt, _ = get_file_ext(self.to_tmp_dir('test_rw_utils.txt'))
         assert file == file_txt + '(1).txt'
