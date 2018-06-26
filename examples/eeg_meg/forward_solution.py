5cb935a1fbf7b4a46f710d61d64ee549c3c672d0
Etienne Combrisson <e.combrisson@gmail.com>
2018-06-26 17:41:33 -0400

    Update eeg_meg folder

--- examples/eeg_meg/forward_solution.py
+++ examples/eeg_meg/forward_solution.py
@@ -4,7 +4,7 @@ Plot forward solution
 
 Plot source estimation.
 
-.. image:: ../../picture/picpysurfer/ex_mne_forward_solution.png
+.. image:: ../../picture/piceegmeg/ex_eegmeg_mne_forward_solution.png
 """
 import os
 
