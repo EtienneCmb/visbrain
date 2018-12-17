f24a4b10be49e777b1b7198fabb883551f126d84
Etienne Combrisson <e.combrisson@gmail.com>
2018-12-17 13:49:11 +0100

    Update release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 from setuptools import setup, find_packages
 
-__version__ = "0.4.3"
+__version__ = "0.4.4"
 NAME = 'visbrain'
 AUTHOR = "Visbrain developpers"
 MAINTAINER = "Etienne Combrisson"
