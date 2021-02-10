79390d31ab80f6a7754341a5b5a154d8924fca28
TomBugnon <tombugnon@hotmail.com>
2021-02-10 17:44:13 +0100

    Update test_rw_hypno

--- visbrain/io/tests/test_rw_hypno.py
+++ visbrain/io/tests/test_rw_hypno.py
@@ -1,11 +1,19 @@
 """Test functions in rw_hypno.py."""
 import numpy as np
+import pytest
 
+from visbrain.io.rw_hypno import (hypno_sample_to_time, hypno_time_to_sample,
+                                  oversample_hypno, read_hypno, write_hypno)
 from visbrain.tests._tests_visbrain import _TestVisbrain
-from visbrain.io.rw_hypno import (hypno_time_to_sample, hypno_sample_to_time,
-                                  oversample_hypno, write_hypno, read_hypno)
 
-versions = dict(time=['.txt', '.csv', '.xlsx'], sample=['.txt', '.hyp'])
+state_cfgs = {
+    'df': (['Art', 'Wake', 'N1', 'N2', 'N3', 'REM'], [-1, 0, 1, 2, 3, 4]),
+    'None': (None, None),
+    'superset': (['Art', 'Wake', 'N1', 'N2', 'N3', 'REM', 'other'],
+                 [-1, 0, 1, 2, 3, 4, 100]),
+    'subset': (['Art', 'Wake'], [-1, 0]),
+    'other': (['1', '2', '3'], [1, 2, 3]),
+}  # (hstates, hvalues)
 
 
 class TestRwHypno(_TestVisbrain):
@@ -15,16 +23,27 @@ class TestRwHypno(_TestVisbrain):
     def _get_hypno():
         return np.array([-1, 4, 2, 3, 0])
 
-    def test_hypno_conversion(self):
+    @pytest.mark.parametrize(
+        "hcfg, hyp", [
+            ('df', [-1, -1, 4, 2, 3, 3, 0, 0, 0, 0, 1, 1, 1, -1, -1]),
+            ('superset', [-1, -1, 4, 2, 3, 3, 0, 0, 0, 0, 1, 1, 1, -1, -1]),
+        ]
+    )
+    def test_hypno_conversion(self, hcfg, hyp):
         """Test conversion functions."""
-        hyp = np.array([-1, -1, 4, 4, 2, 2, 3, 3, 0, 0, 0, 0, 1, 1, 1, -1, -1])
+        hstates, hvalues = state_cfgs[hcfg]
+        hyp = np.array(hyp)
         sf = 100.
         time = np.arange(len(hyp)) / sf
         # Sample -> time :
-        df = hypno_sample_to_time(hyp, time)
+        df = hypno_sample_to_time(hyp, time, hstates, hvalues)
         # Time -> sample :
-        hyp_new, time_new, sf_new = hypno_time_to_sample(df.copy(), len(hyp))
-        hyp_new_2, _, _ = hypno_time_to_sample(df.copy(), time)
+        hyp_new, time_new, sf_new = hypno_time_to_sample(
+            df.copy(), len(hyp), hstates, hvalues
+        )
+        hyp_new_2, _, _ = hypno_time_to_sample(
+            df.copy(), time, hstates, hvalues
+        )
         # Test :
         np.testing.assert_array_equal(hyp, hyp_new)
         np.testing.assert_array_equal(hyp_new, hyp_new_2)
@@ -38,25 +57,55 @@ class TestRwHypno(_TestVisbrain):
         to_hyp = np.array([-1, -1, 4, 4, 2, 2, 3, 3, 0, 0, 0, 0])
         assert np.array_equal(hyp_over, to_hyp)
 
-    def test_write_hypno(self):
+    @pytest.mark.parametrize(
+        "hcfg, hyp, version, ext, expected", [
+
+            # Default states cfg: works with all versions
+            ['None', [-1, -1, 0, 1, 2], 'sample', '.txt', None],
+            ['df', [-1, -1, 0, 1, 2], 'sample', '.txt', None],
+            ['df', [-1, -1, 0, 1, 2], 'sample', '.hyp', None],
+            ['df', [-1, -1, 0, 1, 2], 'time', '.txt', None],
+            ['df', [-1, -1, 0, 1, 2], 'time', '.csv', None],
+            ['df', [-1, -1, 0, 1, 2], 'time', '.xlsx', None],
+            # Check:
+            # - Non default state config can't be saved in elan format
+            # - Hypnogram can't contain non-mapped keys
+            ['subset', [-1, -1, 0, ], 'sample', '.hyp', ValueError],
+            ['subset', [-1, -1, 0, ], 'sample', '.txt', None],
+            ['subset', [-1, -1, 0, ], 'time', '.txt', None],
+            ['subset', [-1, -1, 0, ], 'time', '.csv', None],
+            ['subset', [-1, -1, 0, 1], 'time', '.txt', KeyError],
+            ['superset', [-1, -1, 0, ], 'sample', '.hyp', ValueError],
+            ['superset', [-1, -1, 0, ], 'sample', '.txt', None],
+            ['superset', [-1, -1, 0, ], 'time', '.txt', None],
+            ['superset', [-1, -1, 0, ], 'time', '.csv', None],
+            ['superset', [-2, -1, 0, 1], 'time', '.txt', KeyError],
+            ['other', [1, 1, 3, ], 'sample', '.hyp', ValueError],
+            ['other', [1, 1, 3, ], 'sample', '.txt', None],
+            ['other', [1, 1, 3, ], 'time', '.txt', None],
+            ['other', [1, 1, 3, ], 'time', '.csv', None],
+            ['other', [2, 1, 3, -2], 'time', '.txt', KeyError],
+        ]
+    )
+    def test_rw_hypno(self, hcfg, hyp, version, ext, expected):
         """Test function write_hypno_txt."""
-        hyp = self._get_hypno()
+        hstates, hvalues = state_cfgs[hcfg]
+        hyp = np.array(hyp)
         sf, npts = 1., len(hyp)
         time = np.arange(npts) / sf
         info = {'Info_1': 10, 'Info_2': 'coco', 'Info_3': 'veut_un_gateau'}
-        for k in versions.keys():
-            for e in versions[k]:
-                filename = self.to_tmp_dir('hyp_test_' + k + e)
-                write_hypno(filename, hyp, version=k, sf=sf, npts=npts,
-                            window=1., time=time, info=info)
-
-    def test_read_hypno(self):
-        """Test function read_hypno."""
-        hyp = self._get_hypno()
-        sf, npts = 1., len(hyp)
-        time = np.arange(npts) / sf
-        for k in versions.keys():
-            for e in versions[k]:
-                filename = self.to_tmp_dir('hyp_test_' + k + e)
-                hyp_new, _ = read_hypno(filename, time=time)
-                np.testing.assert_array_equal(hyp, hyp_new)
+        filename = self.to_tmp_dir('hyp_test_' + hcfg + version + ext)
+        if expected is None:
+            # Write
+            write_hypno(filename, hyp, version=version, sf=sf, npts=npts,
+                        window=1., time=time, info=info, hstates=hstates,
+                        hvalues=hvalues, popup=False)
+            # Read
+            hyp_new, _ = read_hypno(filename, time=time, hstates=hstates,
+                                    hvalues=hvalues, popup=False)
+            np.testing.assert_array_equal(hyp, hyp_new)
+        else:
+            with pytest.raises(expected):
+                write_hypno(filename, hyp, version=version, sf=sf, npts=npts,
+                            window=1., time=time, info=info, hstates=hstates,
+                            hvalues=hvalues, popup=False)
